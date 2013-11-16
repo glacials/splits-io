e2815d028a5a653bbe19cac430dca7306328e8fe
skoh-fley <qhiiyr@gmail.com>
2013-11-15 18:15:28 -0800

    Removed upload_post route name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Splitsbin::Application.routes.draw do
   #devise_for :users
   get  "/upload"  => "runs#new",     as: :upload
-  post "/upload"  => "runs#create",  as: :upload_post
+  post "/upload"  => "runs#create"
   get  "/popular" => "runs#popular", as: :popular
   root to: "runs#front"
 end
