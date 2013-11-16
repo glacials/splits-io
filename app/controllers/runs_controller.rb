45b3ec5bca02794fb005eac6d899ff3bb05062d0
skoh-fley <qhiiyr@gmail.com>
2013-11-15 18:52:50 -0800

    Runs are now written to file and saved to the database on upload

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -3,10 +3,22 @@ class RunsController < ApplicationController
   end
   def create
     splits = params[:file]
+    run = Run.create(nick: new_nick, user: nil)
+    File.open(Rails.root.join('private', 'runs', run.nick), 'wb') do |file|
+      file.write(splits.read)
+    end
     # todo: do things with the file
   end
   def popular
   end
   def front
+  end
+
+  def new_nick
+    nick = SecureRandom.urlsafe_base64(4)
+    while Run.find_by(nick: nick).present?
+      nick = SecureRandom.urlsafe_base64(4)
+    end
+    nick
   end
 end
---
+++ private/runs/README.md
@@ -0,0 +1,1 @@
+This directory holds runs uploaded by users.
