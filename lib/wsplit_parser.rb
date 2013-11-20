21921c7ef5bef876efa08ddbdf8951826da538b7
skoh-fley <qhiiyr@gmail.com>
2013-11-19 22:33:16 -0800

    Cleaned up WSplit parser rule names a bit

--- lib/wsplit_parser.rb
+++ lib/wsplit_parser.rb
@@ -3,25 +3,24 @@ class WsplitParser < BabelBridge::Parser
   #rule :rule_name, <list of tokens to match in order>
   # A token can be a string/regex (terminal), or another rule (nonterminal)
   # A question mark after a rule means "optional"
-  rule :wsplit_file, :title_line, :attempts_line, :offset_line, :size_line, :splits_lines, :icons_line
+  rule :wsplit_file, :title_line, :attempts_line, :offset_line, :size_line, many?(:splits), :icons_line
 
   rule :title_line,    "Title=",     :title,    :newline
   rule :attempts_line, "Attempts=",  :attempts, :newline
   rule :offset_line,   "Offset=",    :offset,   :newline
   rule :size_line,     "Size=",      :size,     :newline
-  rule :splits_lines,  many?([:splits_line,     :newline])
+  rule :splits,        :title, ",", :zero, ",", :runtime, ",", :time, :newline
   rule :icons_line,    "Icons=",     /(.*)/,    :newline?
 
-  rule :title,           /([^,\r\n]*)/
-  rule :attempts,        /(\d+)/
-  rule :offset,          /(\d+)/
-  rule :size,            /([^\r\n]*)/
-  rule :splits_line,     :split_title, ",", :zero, ",", :run_time, ",", :split_time
+  rule :title,    /([^,\r\n]*)/
+  rule :attempts, /(\d+)/
+  rule :offset,   /(\d+)/
+  rule :size,     /([^\r\n]*)/
 
-  rule :split_title,     /(([^,\r\n](?!Icons=))*)/
-  rule :zero,            "0"
-  rule :run_time,        /([\d.]+)/
-  rule :split_time,      /([\d.]+)/
+  rule :title,   /([^,\r\n]*)/
+  rule :zero,    "0"
+  rule :runtime, /([\d.]+)/
+  rule :time,    /([\d.]+)/
 
   rule :newline,         :windows_newline
   rule :newline,         :unix_newline
