ff8f5d26875d418fedea10130aa34694b48b0eff
skoh-fley <qhiiyr@gmail.com>
2013-11-20 12:59:40 -0800

    Added a page for "not found" splits

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -9,30 +9,34 @@ class RunsController < ApplicationController
   end
   def show
     @run_record = Run.find_by nick: params[:nick]
-    @run_record.hits += 1
-    @run_record.save
-    splits = File.read Rails.root.join "private", "runs", @run_record.nick
-    wsplit_data           =           WsplitParser.new.parse splits
-    timesplittracker_data = TimesplittrackerParser.new.parse splits
-    splitterz_data        =        SplitterzParser.new.parse splits
-    if wsplit_data.present?
-      @run = wsplit_data
-      render :wsplit
-    elsif timesplittracker_data.present?
-      @run = timesplittracker_data
-      render :timesplittracker
-    elsif splitterz_data.present?
-      @run = splitterz_data
-      render :splitterz
+    if @run_record.nil?
+      render :bad_url
     else
-      if @run_record.hits > 1
-        # If the run has already been viewed (and thus successfully parsed in
-        # the past), we don't want to delete it
-        render "runs/cant_parse"
+      @run_record.hits += 1
+      @run_record.save
+      splits = File.read Rails.root.join "private", "runs", @run_record.nick
+      wsplit_data           =           WsplitParser.new.parse splits
+      timesplittracker_data = TimesplittrackerParser.new.parse splits
+      splitterz_data        =        SplitterzParser.new.parse splits
+      if wsplit_data.present?
+        @run = wsplit_data
+        render :wsplit
+      elsif timesplittracker_data.present?
+        @run = timesplittracker_data
+        render :timesplittracker
+      elsif splitterz_data.present?
+        @run = splitterz_data
+        render :splitterz
       else
-        # If not, we do
-        @run_record.destroy
-        redirect_to cant_parse_path
+        if @run_record.hits > 1
+          # If the run has already been viewed (and thus successfully parsed in
+          # the past), we don't want to delete it
+          render "runs/cant_parse"
+        else
+          # If not, we do
+          @run_record.destroy
+          redirect_to cant_parse_path
+        end
       end
     end
   end
