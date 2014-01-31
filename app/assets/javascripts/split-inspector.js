3cd5ad58e389e3eae55b877518c03dea2aeff73d
Ben Carlsson <bc@twitch.tv>
2014-01-30 20:25:20 -0800

    Refactor/cleanup CSS

--- app/assets/javascripts/split-inspector.js
+++ app/assets/javascripts/split-inspector.js
@@ -1,11 +1,11 @@
 $(function() {
   $('.split').hover(
     function() {
-      $('#inspector-' + this.id.split('-')[1]).fadeTo(0, 1);
+      $('#inspect-' + this.id.split('-')[1]).fadeTo(0, 1);
       $('#time').fadeTo(0, 1);
     },
     function() {
-      $('#inspector-' + this.id.split('-')[1]).fadeTo(0, 0);
+      $('#inspect-' + this.id.split('-')[1]).fadeTo(0, 0);
       $('#time').fadeTo(0, 0);
     }
   );
