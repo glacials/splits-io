c0a640484d9e8711e4a53620db5fb0784da21396
Ben Carlsson <bc@twitch.tv>
2014-02-02 00:06:31 -0800

    Switch to rspec for testing

--- test/helpers/runs_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RunsHelperTest < ActionView::TestCase
-end
--- test/models/run_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RunTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
