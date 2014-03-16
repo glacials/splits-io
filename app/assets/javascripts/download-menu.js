7fbdffa0bb2c82589c2d786f9fa16863780a63db
Ben Carlsson <bc@twitch.tv>
2014-03-15 23:58:20 -0700

    Speed up the download menu appear/disappear a bit

--- app/assets/javascripts/download-menu.js
+++ app/assets/javascripts/download-menu.js
@@ -1,11 +1,11 @@
 function show_download_menu() {
-  $('#download-menu').slideDown();
+  $('#download-menu').slideDown({ duration: 200 });
   setTimeout(hide_download_menu, 20000);
   $('#download-link').html('cancel');
 }
 
 function hide_download_menu() {
-  $('#download-menu').slideUp();
+  $('#download-menu').slideUp({ duration: 200 });
   $('#download-link').html('download');
 }
 
