cd06ed769866680e0b62b203dc30c918f729520a
glacials <bc@twitch.tv>
2014-05-15 01:37:36 -0700

    Force no www subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :remove_www
+
+  def remove_www
+    if request.subdomain == 'www'
+      redirect_to subdomain: nil
+    end
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     request.referrer
   end
