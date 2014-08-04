71239b2a1340cabfbe29eb1622b3ad97dca7fad7
glacials <qhiiyr@gmail.com>
2014-08-04 01:26:41 -0700

    Small code changes

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ SplitsIO::Application.routes.draw do
 
   delete '/:run',        to: 'runs#delete', as: :delete
   post   '/:run/disown', to: 'runs#disown', as: :disown
+
+  get '/games/:game', to: 'games#show'
 end
