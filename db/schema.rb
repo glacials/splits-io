d6c4d1a2d1f68082fbf6992bb188d3b82cac1840
glacials <qhiiyr@gmail.com>
2014-10-27 21:34:45 -0700

    Add workless and delayed_job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925231319) do
+ActiveRecord::Schema.define(version: 20141028042939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 20140925231319) do
 
   add_index "categories", ["game_id"], name: "index_categories_on_game_id", using: :btree
   add_index "categories", ["name"], name: "index_categories_on_name", using: :btree
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "games", force: true do |t|
     t.string   "name"
