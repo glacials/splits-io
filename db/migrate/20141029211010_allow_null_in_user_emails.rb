0eabf4ca0ee4e8ec255c74f24ac76b01a747a31f
glacials <qhiiyr@gmail.com>
2014-10-29 14:11:54 -0700

    Allow null email addresses

---
+++ db/migrate/20141029211010_allow_null_in_user_emails.rb
@@ -0,0 +1,5 @@
+class AllowNullInUserEmails < ActiveRecord::Migration
+  def change
+    change_column :users, :email, :string, null: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028042939) do
+ActiveRecord::Schema.define(version: 20141029211010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20141028042939) do
   add_index "runs", ["user_id"], name: "index_runs_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
