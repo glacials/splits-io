dd799b12fd435f24ec9c482ff676d8b8dc95be73
glacials <qhiiyr@gmail.com>
2015-01-02 00:14:50 -0800

    Minor code cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,8 @@ module ApplicationHelper
   end
 
   def order_runs(runs)
-    col = params[:by].to_sym if [:created_at, :time, :user_id].include?(params[:by].try(:to_sym))
-    dir = params[:order].to_sym if col && [:asc, :desc].include?(params[:order].try(:to_sym))
-    runs.order(col || :created_at => dir || :desc)
+    col = params[:by] if [:created_at, :time, :user_id].map(&:to_s).include?(params[:by])
+    dir = params[:order].to_sym if col && [:asc, :desc].map(&:to_s).include?(params[:order])
+    runs.order((col || :created_at) => (dir || :desc))
   end
 end
