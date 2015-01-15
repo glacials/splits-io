87c035d68db4827fc92e96d8baff9aa9d2e8b6b7
glacials <qhiiyr@gmail.com>
2015-01-15 15:12:07 -0800

    Fix game links mistakenly forwarding to the search page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,7 +8,7 @@ class GamesController < ApplicationController
   private
 
   def set_game
-    @game = Game.find_by(shortname: params[:game_shortname])
+    @game = Game.find_by(shortname: params[:id])
     redirect_to search_path(params[:game_shortname]) if @game.nil?
   end
 end
