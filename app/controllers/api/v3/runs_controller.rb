f9ad9f536a4007f42da2618b94cf0bea9bb4c905
glacials <qhiiyr@gmail.com>
2015-01-19 22:05:44 -0800

    Various `@record` fixes

--- app/controllers/api/v3/runs_controller.rb
+++ app/controllers/api/v3/runs_controller.rb
@@ -8,7 +8,7 @@ class Api::V3::RunsController < Api::V3::ApplicationController
 
   def create
     # TODO: Tokens, not cookies
-    @record = Run.new(file: params.require(:file).read, user: current_user, image_url: params[:image_url]).tap do |run|
+    @run = Run.new(file: params.require(:file).read, user: current_user, image_url: params[:image_url]).tap do |run|
       # TODO: Move this error handling into a before_action or a rescue
       unless run.parses?
         render status: 400, json: {
@@ -39,13 +39,13 @@ class Api::V3::RunsController < Api::V3::ApplicationController
   # TODO: This method is not documented and thus is not officially supported by the API. It uses cookie authentication,
   # but the official release of it into the API should use token authentication.
   def destroy
-    @record.destroy
+    @run.destroy
     head 200
   end
 
   def disown
-    @record.user = nil
-    @record.save
+    @run.user = nil
+    @run.save
     head 200
   end
 
@@ -56,7 +56,7 @@ class Api::V3::RunsController < Api::V3::ApplicationController
   end
 
   def verify_ownership!
-    unless @record.belongs_to?(current_user)
+    unless @run.belongs_to?(current_user)
       render status: 401, json: {
         status: 401,
         message: "Run with id '#{params[:id]}' is not owned by you. You must supply a cookie proving your are the owner of this run in order to disown or delete it."
