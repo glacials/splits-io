57416f46da96bded10af346bebea4544f5a80a0a
glacials <qhiiyr@gmail.com>
2015-01-19 19:06:57 -0800

    Force SSL for API v3, and for the main site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ SplitsIO::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.use_ssl = true
 end
