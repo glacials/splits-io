be3ed5451a9382bf02276ae31710445548a17bea
glacials <qhiiyr@gmail.com>
2015-03-14 21:59:26 -0700

    Fix a couple of issues with #139

--- app/controllers/api/v3/users/games/categories/runs_controller.rb
+++ app/controllers/api/v3/users/games/categories/runs_controller.rb
@@ -5,7 +5,7 @@ class Api::V3::Users::Games::Categories::RunsController < Api::V3::ApplicationCo
   before_action :set_runs, only: [:index]
 
   def index
-    paginate json: @runs, each_serializer: Api::V3::RunsSerializer
+    paginate json: @runs, each_serializer: Api::V3::RunSerializer
   end
 
   private
@@ -30,4 +30,5 @@ class Api::V3::Users::Games::Categories::RunsController < Api::V3::ApplicationCo
 
   def set_runs
     @runs = @user.runs.where(category: @category)
+  end
 end
