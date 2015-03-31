0b936fd1083a02a9da7c38c132fb6f2d8d1bcbe2
glacials <qhiiyr@gmail.com>
2015-03-31 03:58:22 -0700

    Prevent some more JS errors

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -1,10 +1,12 @@
 $(function () {
-  $.each(gon.run.splits, function (i, split) {
-    $('#' + gon.run.id + '-split-' + i).each(function (j, el) {
-      el.style["z-index"] = i;
-      el.style.width = gon.run.splits[i].duration / gon.scale_to * 100 + '%';
+  if (gon.run !== undefined) {
+    $.each(gon.run.splits, function (i, split) {
+      $('#' + gon.run.id + '-split-' + i).each(function (j, el) {
+        el.style["z-index"] = i;
+        el.style.width = gon.run.splits[i].duration / gon.scale_to * 100 + '%';
+      });
     });
-  });
+  }
 
   if (gon.comparison_run !== undefined) {
     $.each(gon.comparison_run.splits, function (i, split) {
