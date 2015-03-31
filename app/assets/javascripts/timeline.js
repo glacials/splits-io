b5cd6d12d4a40c83352db8ee3289fd015e8eedb8
glacials <qhiiyr@gmail.com>
2015-03-31 03:50:46 -0700

    Prevent some JS errors

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -5,10 +5,13 @@ $(function () {
       el.style.width = gon.run.splits[i].duration / gon.scale_to * 100 + '%';
     });
   });
-  $.each(gon.comparison_run.splits, function (i, split) {
-    $('#' + gon.comparison_run.id + '-split-' + i).each(function (j, el) {
-      el.style["z-index"] = i;
-      el.style.width = gon.comparison_run.splits[i].duration / gon.scale_to * 100 + '%';
+
+  if (gon.comparison_run !== undefined) {
+    $.each(gon.comparison_run.splits, function (i, split) {
+      $('#' + gon.comparison_run.id + '-split-' + i).each(function (j, el) {
+        el.style["z-index"] = i;
+        el.style.width = gon.comparison_run.splits[i].duration / gon.scale_to * 100 + '%';
+      });
     });
-  });
+  }
 });
