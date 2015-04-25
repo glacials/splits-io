37d967517045ec3a44870246a2487e804d8957f5
glacials <qhiiyr@gmail.com>
2015-04-25 01:50:24 -0700

    remove outdated AMS `cached` calls

--- app/serializers/api/v3/application_serializer.rb
+++ app/serializers/api/v3/application_serializer.rb
@@ -1,4 +1,3 @@
 class Api::V3::ApplicationSerializer < ActiveModel::Serializer
-  cached
   delegate :cache_key, to: :object
 end
--- app/serializers/api/v4/application_serializer.rb
+++ app/serializers/api/v4/application_serializer.rb
@@ -1,4 +1,3 @@
 class Api::V4::ApplicationSerializer < ActiveModel::Serializer
-  cached
   delegate :cache_key, to: :object
 end
