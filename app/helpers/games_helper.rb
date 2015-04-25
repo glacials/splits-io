3d77d030e468ee3e9b222f66a84fc67ab089f6fa
glacials <qhiiyr@gmail.com>
2015-04-25 00:18:52 -0700

    Fix games#index 500s when 0 shortnamed games exist
    Also separate the placeholder finder line into a helper. Fixes #157

---
+++ app/helpers/games_helper.rb
@@ -0,0 +1,11 @@
+module GamesHelper
+  def placeholder
+    random_placeholder || Game.new(name: 'Super Mario Sunshine', shortname: 'sms')
+  end
+
+  private
+
+  def random_placeholder
+    Game.where.not(shortname: nil).offset(rand(Game.where.not(shortname: nil).count)).first
+  end
+end
