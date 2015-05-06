29ecdbdd90ee81f638a4f75c55562b5e4f0f31ee
glacials <qhiiyr@gmail.com>
2015-05-06 00:29:39 -0700

    Update schema

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,9 +1,3 @@
-require 'livesplit_parser'
-require 'splitterz_parser'
-require 'time_split_tracker_parser'
-require 'wsplit_parser'
-require 'urn_parser'
-
 class Run < ActiveRecord::Base
   include PadawanRun
   include ForgetfulPersonsRun
