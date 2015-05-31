abbe56b6cf452c0dbe9fe8bec7744fa0071aaf86
glacials <qhiiyr@gmail.com>
2015-05-30 18:07:40 -0700

    Fix 500s on /api/v3/runs/:id

--- app/serializers/api/v3/detail/run_serializer.rb
+++ app/serializers/api/v3/detail/run_serializer.rb
@@ -1,8 +1,7 @@
 class Api::V3::Detail::RunSerializer < Api::V3::ApplicationSerializer
-  has_one :user, serializer: Api::V3::UserSerializer
-  has_one :category, serializer: Api::V3::CategorySerializer
+  belongs_to :user, serializer: Api::V3::UserSerializer
+  belongs_to :category, serializer: Api::V3::CategorySerializer
   has_one :time
-  has_many :splits
 
-  attributes :id, :path, :name, :program, :image_url, :created_at, :updated_at
+  attributes :id, :path, :name, :program, :image_url, :created_at, :updated_at, :splits
 end
