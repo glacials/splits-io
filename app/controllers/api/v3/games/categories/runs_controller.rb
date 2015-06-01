glacials/splits-io

commit 4e4129008e1bf57f76cf9a565a939558861db7b9

Add roots back to ActiveModel::Serializer responses. Fixes #175
AMS 0.10.x seems to change default behavior to no root. Idea for the application-wide fix from
https://github.com/rails-api/active_model_serializers/issues/898

config/initializers/active_model_serializers.rb
@@ -1,0 +1,7 @@
+class ActiveModel::Serializer::Adapter::Json
+  def initialize_with_root(serializer, options = {})
+    initialize_without_root(serializer, options)
+    serializer.root = true
+  end
+  alias_method_chain :initialize, :root
+end

commit 025049dd8822f43fc1ec95e87949d1cbd100d7d0

Manually specify APIv3 json root nodes
That last hack in 4e4129008e1bf57f76cf9a565a939558861db7b9 caused array responses to have several roots

app/controllers/api/v3/games/categories/runs_controller.rb
@@ -4,7 +4,7 @@ class Api::V3::Games::Categories::RunsController < Api::V3::ApplicationControlle
   before_action :set_runs, only: [:index]
 
   def index
-    paginate json: @runs, each_serializer: Api::V3::RunSerializer
+    paginate json: @runs, each_serializer: Api::V3::RunSerializer, root: :runs
   end
 
   def create

app/controllers/api/v3/games/categories_controller.rb
@@ -3,7 +3,7 @@ class Api::V3::Games::CategoriesController < Api::V3::ApplicationController
   before_action :set_category, only: [:show]
 
   def show
-    render json: @category, serializer: Api::V3::CategorySerializer
+    render json: @category, serializer: Api::V3::CategorySerializer, root: :category
   end
 
   private

app/controllers/api/v3/games/runs_controller.rb
@@ -3,7 +3,7 @@ class Api::V3::Games::RunsController < Api::V3::ApplicationController
   before_action :set_runs, only: [:index]
 
   def index
-    paginate json: @runs, each_serializer: Api::V3::RunSerializer
+    paginate json: @runs, each_serializer: Api::V3::RunSerializer, root: :runs
   end
 
   private

app/controllers/api/v3/games_controller.rb
@@ -7,11 +7,11 @@ class Api::V3::GamesController < Api::V3::ApplicationController
       return
     end
     @games = Game.search(params[:search]).includes(:categories)
-    render json: @games, each_serializer: Api::V3::GameSerializer
+    render json: @games, each_serializer: Api::V3::GameSerializer, root: :games
   end
 
   def show
-    render json: @game, serializer: Api::V3::GameSerializer
+    render json: @game, serializer: Api::V3::GameSerializer, root: :game
   end
 
   private

app/controllers/api/v3/runs_controller.rb
@@ -3,7 +3,7 @@ class Api::V3::RunsController < Api::V3::ApplicationController
   before_action :verify_ownership!, only: [:destroy, :disown]
 
   def show
-    render json: @run, serializer: Api::V3::Detail::RunSerializer
+    render json: @run, serializer: Api::V3::Detail::RunSerializer, root: :run
   end
 
   def create

app/controllers/api/v3/users/games/categories/pbs_controller.rb
@@ -5,7 +5,7 @@ class Api::V3::Users::Games::Categories::PbsController < Api::V3::ApplicationCon
   before_action :set_pb, only: [:show]
 
   def show
-    render json: @pb, serializer: Api::V3::Detail::RunSerializer
+    render json: @pb, serializer: Api::V3::Detail::RunSerializer, root: :run
   end
 
   private

app/controllers/api/v3/users/games/categories/runs_controller.rb
@@ -5,7 +5,7 @@ class Api::V3::Users::Games::Categories::RunsController < Api::V3::ApplicationCo
   before_action :set_runs, only: [:index]
 
   def index
-    paginate json: @runs, each_serializer: Api::V3::RunSerializer
+    paginate json: @runs, each_serializer: Api::V3::RunSerializer, root: :runs
   end
 
   private

app/controllers/api/v3/users/pbs_controller.rb
@@ -3,7 +3,7 @@ class Api::V3::Users::PbsController < Api::V3::ApplicationController
   before_action :set_pbs, only: [:index]
 
   def index
-    render json: @pbs, each_serializer: Api::V3::RunSerializer
+    render json: @pbs, each_serializer: Api::V3::RunSerializer, root: :pbs
   end
 
   private

app/controllers/api/v3/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V3::UsersController < Api::V3::ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    render json: @user, serializer: Api::V3::UserSerializer
+    render json: @user, serializer: Api::V3::UserSerializer, root: :user
   end
 
   private

config/initializers/active_model_serializers.rb
@@ -1,10 +1,0 @@
-module ActiveModel::Serializer::EnableRootNodesGlobally
-  def initialize(serializer, options = {})
-    super(serializer, options)
-    serializer.root = true
-  end
-end
-
-class ActiveModel::Serializer::Adapter::Json
-  prepend ActiveModel::Serializer::EnableRootNodesGlobally
-end