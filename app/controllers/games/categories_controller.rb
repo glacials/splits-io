b794360738b6ff8afe7bf6c5c7d290d07da97b45
glacials <qhiiyr@gmail.com>
2015-06-07 17:48:55 -0700

    Turbolinks partial replacements in run#edit

--- app/controllers/games/categories_controller.rb
+++ app/controllers/games/categories_controller.rb
@@ -3,10 +3,6 @@ class Games::CategoriesController < ApplicationController
   before_action :set_category
 
   def show
-    # temporary
-    @category.runs.each do |run|
-      run.delay.refresh_from_file if rand < SplitsIO::Application.config.run_refresh_chance
-    end
   end
 
   private
