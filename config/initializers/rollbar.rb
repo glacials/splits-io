b625b5d057773495a69857b8160d82fe6eb797c9
glacials <qhiiyr@gmail.com>
2015-06-15 23:14:14 -0700

    Stop reporting 404s to Rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,36 +1,11 @@
 require 'rollbar/rails'
 Rollbar.configure do |config|
-  # Without configuration, Rollbar is enabled in all environments.
-  # To disable in specific environments, set config.enabled=false.
-
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
-  # Here we'll disable in 'test':
-  if ['development', 'hot', 'test'].include?(Rails.env)
-    config.enabled = false
-  end
+  config.enabled = (Rails.env == 'production')
 
   config.person_method = 'current_user'
   config.person_id_method = 'id'
   config.person_username_method = 'name'
-
-  # If you want to attach custom data to all exception and message reports,
-  # provide a lambda like the following. It should return a hash.
-  # config.custom_data_method = lambda { {:some_key => "some_value" } }
-
-  # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
-  # is not installed)
-  # config.use_async = true
-  # Supply your own async handler:
-  # config.async_handler = Proc.new { |payload|
-  #  Thread.new { Rollbar.process_payload(payload) }
-  # }
-
-  # Enable asynchronous reporting (using sucker_punch)
-  # config.use_sucker_punch
-
-  # Enable delayed reporting (using Sidekiq)
-  # config.use_sidekiq
-  # You can supply custom Sidekiq options:
-  # config.use_sidekiq 'queue' => 'my_queue'
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
 end
