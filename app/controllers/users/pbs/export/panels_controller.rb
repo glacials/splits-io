670582121ef8f4616e1ade19f05dc93f41d93d4e
glacials <qhiiyr@gmail.com>
2015-07-15 00:09:20 -0700

    Allow custom sorting of Twitch panel exports

--- app/controllers/users/pbs/export/panels_controller.rb
+++ app/controllers/users/pbs/export/panels_controller.rb
@@ -1,5 +1,6 @@
 class Users::Pbs::Export::PanelsController < ApplicationController
   before_action :set_user
+  before_action :set_runs
 
   def index
     render layout: false, content_type: 'text/plain'
@@ -9,5 +10,9 @@ class Users::Pbs::Export::PanelsController < ApplicationController
 
   def set_user
     @user = User.find_by!(name: params[:user_id])
+  end
+
+  def set_runs
+    @runs = @user.pbs.categorized
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 
   def order_runs(runs)
-    col = params[:by] if [:created_at, :time, :user_id].map(&:to_s).include?(params[:by])
+    col = params[:by] if [:created_at, :time, :user_id, :name].map(&:to_s).include?(params[:by])
     dir = params[:order].to_sym if col && [:asc, :desc].map(&:to_s).include?(params[:order])
     runs.order((col || :created_at) => (dir || :desc))
   end
