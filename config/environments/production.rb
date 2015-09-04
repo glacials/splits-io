0067d43c3313deaee5f38cda2ce80c5b144a1e4b
glacials <qhiiyr@gmail.com>
2015-09-03 23:36:59 -0700

    Use memcached for caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,15 @@ SplitsIO::Application.configure do
   config.consider_all_requests_local       = false
 
   config.action_controller.perform_caching = true
-  config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store,
+    (ENV['MEMCACHED_SERVERS'] || '').split(','),
+    {
+      username: ENV['MEMCACHED_USERNAME'],
+      password: ENV['MEMCACHED_PASSWORD'],
+      failover: true,
+      socket_timeout: 1.5,
+      socket_failure_delay: 0.2
+    }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
