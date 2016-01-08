e95ba470b71800a10c1b11b2ceb6ecc1a14a5d93
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-08 04:54:15 -0800

    updated serialzier to match run output in docs

--- app/serializers/api/v4/convert/run_serializer.rb
+++ app/serializers/api/v4/convert/run_serializer.rb
@@ -1,5 +1,9 @@
 class Api::V4::Convert::RunSerializer < Api::V4::ApplicationSerializer
-  attributes :id, :srdc_id, :name, :time, :program, :image_url, :video_url, :created_at, :updated_at, :splits
+  has_one :game, serializer: Api::V4::GameSerializer
+  has_one :category, serializer: Api::V4::CategorySerializer
+  has_many :runners, serializer: Api::V4::RunnerSerializer
+
+  attributes :id, :srdc_id, :name, :time, :program, :attempts, :image_url, :video_url, :created_at, :updated_at, :splits
 
   def created_at
     DateTime.now.utc.to_s(:iso8601)
@@ -11,5 +15,9 @@ class Api::V4::Convert::RunSerializer < Api::V4::ApplicationSerializer
 
   def history
     object.history || []
+  end
+
+  def attempts
+    object.attempts || 0
   end
 end
