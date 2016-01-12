2846c70f96fe13c3ea09395dd6e7364981308e6f
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-12 00:45:37 -0800

    Add test for unsupported formats

--- spec/controllers/api/v4/converts_controller_spec.rb
+++ spec/controllers/api/v4/converts_controller_spec.rb
@@ -4,25 +4,37 @@ describe Api::V4::ConvertsController do
 
   describe '#create' do
     context 'from Llanfair' do
+      context 'supported format'
+        let(:file) do
+          fixture_file_upload('files/llanfair')
+        end
+        subject { post :create, params: {file: file, format: "json", historic: "on"} }
+        let(:body) { JSON.parse(subject.body) }
+
+        it "returns a 200" do
+          expect(subject).to have_http_status 200
+        end
+
+        it "doesn't include id" do
+          expect(body['id']).to be_nil
+        end
+
+        it "has the correct splits" do
+          expect(body["splits"].map { |s| [s["name"], s["duration"]] }).to eq([
+            ["Spiral Mountain", 211.23],
+            ["Mumbo's Mountain", 808.2]
+          ])
+        end
+      end
+
+    context 'unsupported format' do
       let(:file) do
         fixture_file_upload('files/llanfair')
       end
-      subject { post :create, params: {file: file, program: "splits_io", include_history: "on"} }
-      let(:body) { JSON.parse(subject.body) }
+      subject { post :create, params: {file: file, format: "llanfair" } }
 
-      it "retruns a 200" do
-        expect(subject).to have_http_status 200
-      end
-
-      it "doesn't include id" do
-        expect(body['id']).to be_nil
-      end
-
-      it 'has the correct splits' do
-        expect(body["splits"].map { |s| [s["name"], s["duration"]] }).to eq([
-          ["Spiral Mountain", 211.23],
-          ["Mumbo's Mountain", 808.2]
-        ])
+      it "returns a 400" do
+        expect(subject).to have_http_status 400
       end
     end
   end
