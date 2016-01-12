1785ce4d3faf698df78e3c20cc752655f9e1233a
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-01-12 00:44:45 -0800

    Change program=>format and add before_action
    Switch to using parse as well with new optional parameter

--- app/controllers/api/v4/converts_controller.rb
+++ app/controllers/api/v4/converts_controller.rb
@@ -1,8 +1,8 @@
 class Api::V4::ConvertsController < Api::V4::ApplicationController
 
+  before_action :check_parameters, only: [:create]
+
   def create
-    params.require(:file)
-    params.require(:program)
     if params[:file].respond_to?(:read)
       file_text = params[:file].read
       if file_text[8..29] == "org.fenix.llanfair.Run"
@@ -13,23 +13,36 @@ class Api::V4::ConvertsController < Api::V4::ApplicationController
     end
 
     @run = Run.new(run_file: run_file, user: nil)
-    @run.convert(fast: params[:include_history] == "on" ? true : false)
+    @run.parse(fast: ["on", "1"].include?(params[:historic]) ? true : false, convert: true)
 
     extension = {'livesplit' => 'lss',
       'urn' => 'json',
-      'splits_io' => 'json'
-    }[params[:program]] || params[:program]
+    }[params[:format]] || params[:format]
 
-    if params[:program] == "splits_io"
+    if params[:format] == "json"
       new_file = render_to_string(json: @run, serializer: Api::V4::Convert::RunSerializer)
     else
-      new_file = render_to_string("runs/#{params[:program]}", layout: false)
+      new_file = render_to_string("runs/#{params[:format]}", layout: false)
     end
     send_data(
       new_file,
       filename: "#{params[:file].original_filename.split(".")[0..-1].join}.#{extension}",
       layout: false
     )
+  end
+
+  private
+
+  def check_parameters
+    params.require(:file)
+    params.require(:format)
+    supported = (Run.programs - [Llanfair]) + ["json"]
+    unless supported.include?(params[:format])
+      render status: 400, json: {
+        status: 400,
+        message: "Convert supports the following formats: #{supported}"
+      }
+    end
   end
 
 end
