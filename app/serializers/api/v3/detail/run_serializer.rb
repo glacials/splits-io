bcdba3dd5c6c1a0eeda926600d76a02e6678cc7e
glacials <qhiiyr@gmail.com>
2016-07-22 00:17:05 -0700

    Add attempts and sum_of_best to api v3

--- app/serializers/api/v3/detail/run_serializer.rb
+++ app/serializers/api/v3/detail/run_serializer.rb
@@ -4,12 +4,16 @@ class Api::V3::Detail::RunSerializer < Api::V3::ApplicationSerializer
   belongs_to :category, serializer: Api::V3::CategorySerializer
   has_one :time
 
-  attributes :id, :path, :name, :program, :image_url, :created_at, :updated_at, :video_url, :splits
+  attributes :id, :path, :name, :program, :image_url, :created_at, :updated_at, :video_url, :splits, :attempts, :sum_of_best
 
   def splits
     object.splits.map do |segment|
       segment.best = {duration: segment.best}
       segment
     end
+  end
+
+  def sum_of_best
+    object.sum_of_best.to_f
   end
 end
--- app/serializers/api/v3/run_serializer.rb
+++ app/serializers/api/v3/run_serializer.rb
@@ -4,5 +4,9 @@ class Api::V3::RunSerializer < Api::V3::ApplicationSerializer
   belongs_to :category, serializer: Api::V3::CategorySerializer
   has_one :time
 
-  attributes :id, :path, :name, :program, :image_url, :created_at, :updated_at, :video_url
+  attributes :id, :path, :name, :program, :image_url, :created_at, :updated_at, :video_url, :attempts, :sum_of_best
+
+  def sum_of_best
+    object.sum_of_best.to_f
+  end
 end
