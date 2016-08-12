7cf4cc9088966649a706fcb64cbcad714665e00f
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2016-08-12 09:02:34 -0700

    Move all the graphs to the stats page

--- app/controllers/runs/stats_controller.rb
+++ app/controllers/runs/stats_controller.rb
@@ -1,8 +1,19 @@
 class Runs::StatsController < Runs::ApplicationController
+  include RunsHelper
   before_action :set_run, only: [:index]
 
   def index
-    gon.run = {id: @run.id, splits: @run.collapsed_splits}
+    @run.parse(fast: false)
+    gon.run = {
+      id: @run.id,
+      splits: @run.collapsed_splits,
+      raw_splits: @run.parse(fast: false)[:splits],
+      history: @run.history,
+      attempts: @run.attempts,
+      program: @run.program,
+      colors: GRAPH_COLORS
+    }
     gon.scale_to = @run.time
+
   end
 end
