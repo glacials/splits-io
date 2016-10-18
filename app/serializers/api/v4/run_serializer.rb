9245addeb567cb34140d44fa2bcf3ccf7bcb99b7
glacials <qhiiyr@gmail.com>
2016-10-18 09:40:59 -0700

    Remove name from API v4 runs

--- app/serializers/api/v4/run_serializer.rb
+++ app/serializers/api/v4/run_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V4::RunSerializer < Api::V4::ApplicationSerializer
-  attributes :id, :srdc_id, :name, :time, :program, :attempts, :image_url, :created_at, :updated_at, :video_url
+  attributes :id, :srdc_id, :time, :program, :attempts, :image_url, :created_at, :updated_at, :video_url
 
   has_one :game, serializer: Api::V4::GameSerializer
   has_one :category, serializer: Api::V4::CategorySerializer
