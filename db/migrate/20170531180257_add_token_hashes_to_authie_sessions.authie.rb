c6cd424000e59d4e31831acc4f57fe27cd9ecd26
glacials <qhiiyr@gmail.com>
2017-05-31 15:33:08 -0700

    Revert "Update authie"
    This reverts commit 34c26332d6913f9ff8e3869d00b8e18d82fb7f04.

--- db/migrate/20170531180257_add_token_hashes_to_authie_sessions.authie.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from authie (originally 20170417170000)
-class AddTokenHashesToAuthieSessions < ActiveRecord::Migration
-  def change
-    add_column :authie_sessions, :token_hash, :string
-  end
-end
--- db/migrate/20170531180258_add_index_to_token_hashes_on_authie_sessions.authie.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from authie (originally 20170421174100)
-class AddIndexToTokenHashesOnAuthieSessions < ActiveRecord::Migration
-  def change
-    add_index :authie_sessions, :token_hash
-  end
-end
