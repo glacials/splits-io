578e6125bd1aa03c36222439c6b4d9f89eb391e2
glacials <qhiiyr@gmail.com>
2017-06-12 10:43:50 -0700

    Fix some convert bugs

--- app/serializers/api/v4/convert/run_serializer.rb
+++ app/serializers/api/v4/convert/run_serializer.rb
@@ -5,6 +5,10 @@ class Api::V4::Convert::RunSerializer < Api::V4::ApplicationSerializer
 
   attributes :id, :srdc_id, :time, :program, :attempts, :image_url, :video_url, :created_at, :updated_at, :splits
 
+  def id
+    nil
+  end
+
   def created_at
     DateTime.now.utc.to_s(:iso8601)
   end
@@ -30,6 +34,23 @@ class Api::V4::Convert::RunSerializer < Api::V4::ApplicationSerializer
   end
 
   def splits
-    object.parse[:splits]
+    object.segments.map do |segment|
+      segment.shortest_duration_milliseconds = {duration: segment.shortest_duration_milliseconds / 1000}
+      segment
+      {
+        name: segment.name,
+        duration: (segment.duration_milliseconds || 0).to_f / 1000,
+        finish_time: (segment.end_milliseconds || 0).to_f / 1000,
+        best: {
+          duration: (segment.shortest_duration_milliseconds || 0).to_f / 1000
+        },
+        history: segment.histories.map do |history|
+          (history.duration_milliseconds || 0).to_f / 1000
+        end,
+        gold: segment.gold?,
+        skipped: segment.skipped?,
+        reduced: segment.reduced?
+      }
+    end
   end
 end
