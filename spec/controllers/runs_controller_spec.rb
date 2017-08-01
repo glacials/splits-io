1c386d7a231f8f7c33d3b3e4bcd21ca51bc76d9d
glacials <qhiiyr@gmail.com>
2017-08-01 13:42:39 -0700

    Add tests for downloading runs

--- spec/controllers/runs_controller_spec.rb
+++ spec/controllers/runs_controller_spec.rb
@@ -251,4 +251,49 @@ describe RunsController do
       end
     end
   end
+
+  describe '#download' do
+    let(:run) { FactoryGirl.create(:run, :parsed) }
+    let(:response) { get(:download, params: {run: run.id36, timer: timer}) }
+
+    context 'as Urn' do
+      let(:timer) { 'urn' }
+
+      it 'returns a 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'as LiveSplit' do
+      let(:timer) { 'livesplit' }
+
+      it 'returns a 302' do
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'as SplitterZ' do
+      let(:timer) { 'splitterz' }
+
+      it 'returns a 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'as Time Split Tracker' do
+      let(:timer) { 'splitterz' }
+
+      it 'returns a 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'as WSplit' do
+      let(:timer) { 'wsplit' }
+
+      it 'returns a 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
---
+++ spec/views/runs/download_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'runs/urn' do
+  it 'renders the Urn template' do
+    assign(:run, FactoryGirl.create(:run, :parsed))
+    render
+
+    expect(view).to render_template('runs/urn')
+  end
+end
+
+RSpec.describe 'runs/livesplit' do
+  it 'renders the LiveSplit template' do
+    assign(:run, FactoryGirl.create(:run, :parsed))
+    render
+
+    expect(view).to render_template('runs/livesplit')
+  end
+end
+
+RSpec.describe 'runs/splitterz' do
+  it 'renders the SplitterZ template' do
+    assign(:run, FactoryGirl.create(:run, :parsed))
+    render
+
+    expect(view).to render_template('runs/splitterz')
+  end
+end
+
+RSpec.describe 'runs/timesplittracker' do
+  it 'renders the Time Split Tracker template' do
+    assign(:run, FactoryGirl.create(:run, :parsed))
+    render
+
+    expect(view).to render_template('runs/timesplittracker')
+  end
+end
+
+RSpec.describe 'runs/wsplit' do
+  it 'renders the WSplit template' do
+    assign(:run, FactoryGirl.create(:run, :parsed))
+    render
+
+    expect(view).to render_template('runs/wsplit')
+  end
+end
