e09643cf1765892b9012b827af46a26cc2d2e756
glacials <qhiiyr@gmail.com>
2017-08-09 00:12:37 -0700

    Fix tests

--- spec/controllers/rivalries_controller_spec.rb
+++ spec/controllers/rivalries_controller_spec.rb
@@ -5,7 +5,7 @@ describe Users::RivalriesController do
     user = FactoryGirl.create(:user, :with_runs)
     user.categories.each do |category|
       FactoryGirl.create(:run, :parsed, category: category, user: FactoryGirl.create(:user))
-      FactoryGirl.create(:run, :attempteless, category: category, user: FactoryGirl.create(:user))
+      FactoryGirl.create(:run, :attemptless, category: category, user: FactoryGirl.create(:user))
     end
 
     let(:response) { get(:index, params: {user: user}) }
