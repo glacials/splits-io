d710b086e56e565030bbcbd228f95015ef1cefca
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-10-19 01:21:55 -0700

    Use do ... end kinda

--- app/models/run_history.rb
+++ app/models/run_history.rb
@@ -1,14 +1,14 @@
 class RunHistory < ApplicationRecord
   belongs_to :run
 
-  scope :completed, lambda { |timing|
+  scope :completed, (lambda do |timing|
     case timing
     when Run::REAL
       where('realtime_duration_ms > 0').count
     when Run::GAME
       where('gametime_duration_ms > 0').count
     end
-  }
+  end)
 
   def duration_ms(time_type)
     case time_type
