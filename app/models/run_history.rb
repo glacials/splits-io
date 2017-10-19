357c2d91a7910774eb259b1ab96abf0b8f76d588
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2017-10-19 14:42:57 -0700

    Convert scope to class method

--- app/models/run_history.rb
+++ app/models/run_history.rb
@@ -1,14 +1,14 @@
 class RunHistory < ApplicationRecord
   belongs_to :run
 
-  scope :completed, (lambda do |timing|
+  def self.completed(timing)
     case timing
     when Run::REAL
-      where('realtime_duration_ms > 0').count
+      RunHistory.where('realtime_duration_ms > 0')
     when Run::GAME
-      where('gametime_duration_ms > 0').count
+      RunHistory.where('gametime_duration_ms > 0')
     end
-  end)
+  end
 
   def duration_ms(time_type)
     case time_type
