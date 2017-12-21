cc9666bf727f16371a9427638430a9136db7ed32
glacials <qhiiyr@gmail.com>
2017-12-21 15:35:49 -0800

    remove semicolons

--- scripts/lambda/index.js
+++ scripts/lambda/index.js
@@ -1,43 +1,43 @@
-"use strict";
-const lsc = require('./livesplit_core');
-const fs = require('fs');
+"use strict"
+const lsc = require('./livesplit_core')
+const fs = require('fs')
 
-const pg = require('pg');
-const db = new pg();
-db.connect();
+const { Client } = require('pg')
+const db = new Client()
+db.connect()
 
-const aws = require('aws-sdk');
-const s3 = new aws.S3();
+const aws = require('aws-sdk')
+const s3 = new aws.S3()
 
 exports.handler = function (event, context, callback) {
-  console.log(event);
+  console.log(event)
   const promises = event['Records'].map((record) => {
     return new Promise((resolve, reject) => {
-      const key = record['s3']['object']['key'];
-      const params = {Bucket: 'splits.io', Key: key};
+      const key = record['s3']['object']['key']
+      const params = {Bucket: 'splits.io', Key: key}
 
       s3.getObject(params, (err, data) => {
         if (err) {
-          return reject(err);
+          return reject(err)
         }
 
-        client.query('SELECT id FROM runs WHERE s3_filename = $1::text', [key], (err, res) => {
+        db.query('SELECT id FROM runs WHERE s3_filename = $1::text', [key], (err, res) => {
           console.log(err ? err.stack : res.rows[0].id)
         })
 
-        const result = lsc.Run.parseArray(data['Body'], '', false);
-        console.log(`parsed: ${result.parsedSuccessfully()}`);
+        const result = lsc.Run.parseArray(data['Body'], '', false)
+        console.log(`parsed: ${result.parsedSuccessfully()}`)
         if (result.parsedSuccessfully()) {
-          console.log(`timer: ${result.timerKind()}`);
-          const run = result.unwrap();
-          console.log(`game: ${run.gameName()}`);
-          resolve();
+          console.log(`timer: ${result.timerKind()}`)
+          const run = result.unwrap()
+          console.log(`game: ${run.gameName()}`)
+          resolve()
         }
-      });
-    });
-  });
+      })
+    })
+  })
 
   Promise.all(promises).then(function () {
-    console.log("Complete!");
-  }).catch(console.error);
-};
+    console.log("Complete!")
+  }).catch(console.error)
+}
