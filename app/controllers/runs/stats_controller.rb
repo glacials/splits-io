b127cc6c17f7240e2c1192198fb503161614755d
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-02-14 14:41:11 -0800

    Optimize stats page (#348)
    27 queries -> 14, timing switch render times >200ms -> <10ms

--- app/controllers/runs/stats_controller.rb
+++ app/controllers/runs/stats_controller.rb
@@ -10,50 +10,35 @@ class Runs::StatsController < Runs::ApplicationController
       render 'runs/cant_parse', status: 500
     end
 
-    segments = @run.segments.includes(:histories).map do |segment|
-      segment.attributes.merge(history: segment.histories)
-    end
-
     gon.run = {
       id: @run.id36,
-      segments: @run.segments.map do |segment|
+      segments: @run.segments.includes(:histories).map do |segment|
         {
           name: segment.name,
           duration_ms: segment.duration_ms(timing),
           shortest_duration_ms: segment.shortest_duration_ms(timing),
           histories: segment.histories.map do |history|
             {
-              duration_ms: history.duration_ms(timing),
+              duration_ms: history.duration_ms(timing)
             }
-          end,
+          end
         }
       end,
       history: @run.histories.select(:id, :attempt_number, :realtime_duration_ms, :gametime_duration_ms),
       attempts: @run.attempts,
       program: @run.program
     }
-
-    if @run.user.nil?
-      gon.run['user'] = nil
-    else
-      gon.run['user'] = {
-        id: @run.user.id,
-        name: @run.user.name
-      }
-    end
-
-    gon.scale_to = @run.duration_ms(timing)
   end
 
   def run_history_csv
     @run.parse_into_db unless @run.parsed?
 
-    column_names = ["Attempt #", "Realtime (ms)", "Gametime (ms)"]
+    column_names = ['Attempt #', 'Realtime (ms)', 'Gametime (ms)']
 
-    csv = CSV.generate do |csv|
-      csv << column_names
+    csv = CSV.generate do |doc|
+      doc << column_names
       @run.histories.each do |history|
-        csv << [history.attempt_number, history.realtime_duration_ms, history.gametime_duration_ms]
+        doc << [history.attempt_number, history.realtime_duration_ms, history.gametime_duration_ms]
       end
     end
 
@@ -63,14 +48,12 @@ class Runs::StatsController < Runs::ApplicationController
   def segment_history_csv
     @run.parse_into_db unless @run.parsed?
 
-    if @run.attempts.nil? || @run.attempts == 0
-      redirect_to run_stats_path(@run), alert: "Segment history is not available for this run."
+    if @run.attempts.nil? || @run.attempts.zero?
+      redirect_to run_stats_path(@run), alert: 'Segment history is not available for this run.'
       return
     end
 
-    csv = CSV.generate do |csv|
-      rows = []
-
+    csv = CSV.generate do |doc|
       header = ['Segment name']
 
       if @run.segments.empty? || @run.segments.first.histories.empty?
@@ -82,25 +65,23 @@ class Runs::StatsController < Runs::ApplicationController
       (1..attempts).each do |attempt_number|
         header << "Attempt ##{attempt_number}'s Duration (ms)"
       end
-      csv << header
+      doc << header
 
       @run.segments.order(segment_number: :asc).includes(:histories).each do |segment|
         row = []
         row << segment.name
-        if segment.histories.empty?
-          next
-        end
+        next if segment.histories.empty?
 
         attempt_number = 1
         segment.histories.order(attempt_number: :asc).each do |h|
           while h.attempt_number > attempt_number
-            row << ""
+            row << ''
             attempt_number += 1
           end
           row << h.duration_ms(Run::REAL)
           attempt_number += 1
         end
-        csv << row
+        doc << row
       end
     end
 
