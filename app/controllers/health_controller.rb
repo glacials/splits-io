472bba2f39ff797d3ea8643fc7c110f404039f78
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-03-27 15:34:32 -0700

    Optimize api stuff (#362)
    * Fix n+1 -> 1 queries
    
    * Skip authie check on api calls (14% of cpu time was spent on this)
    
    * Add authie skip to health controller

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,5 +1,7 @@
 class HealthController < ApplicationController
   force_ssl except: [:index]
+  skip_before_action :set_browser_id
+  skip_before_action :touch_auth_session
 
   def index
     head 200
