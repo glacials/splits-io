8bf0ff8b154bdba8fea73d356c558776d10e58dd
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-05-02 23:22:17 -0700

    Remove select box and search type, search on all models
    this also removes the run table that would show when only games where being searched on

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,26 +1,17 @@
 class SearchController < ApplicationController
   before_action :set_query, only: [:index]
-  before_action :set_search_type, only: [:index]
 
   def index
     return if @query.blank?
-    return if @search_type.blank?
 
-    case @search_type
-    when 'game'
-      @results = Game.search(@query).order(:shortname, :name).includes(:categories)
-    when 'user'
-      @results = User.search(@query).includes(:games)
-    end
+    @results = {}
+    @results[:games] = Game.search(@query).order(:shortname, :name).includes(:categories)
+    @results[:users] = User.search(@query).includes(:games)
   end
 
   private
 
   def set_query
     @query = params[:q].strip if params[:q].present?
-  end
-
-  def set_search_type
-    @search_type = params[:search_type]
   end
 end
