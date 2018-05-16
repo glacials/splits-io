3ed0524fce4964df6cad018af96259e15aad70a0
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-05-16 02:11:57 -0700

    add index to users twitch_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180508193227) do
+ActiveRecord::Schema.define(version: 20180516090856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -261,6 +261,7 @@ ActiveRecord::Schema.define(version: 20180508193227) do
     t.datetime "twitch_user_follows_checked_at"
     t.index ["email"], name: "index_users_on_email"
     t.index ["name"], name: "index_users_on_name"
+    t.index ["twitch_id"], name: "index_users_on_twitch_id"
   end
 
   add_foreign_key "game_aliases", "games", on_delete: :cascade
