a698d6cc81e9815f1639c9f83e4ca48fe7d877b7
glacials <qhiiyr@gmail.com>
2018-06-28 10:56:31 -0700

    Kill subscriptions when leaving the page

--- app/assets/javascripts/cable/subscriptions/run.js
+++ app/assets/javascripts/cable/subscriptions/run.js
@@ -1,7 +1,10 @@
 $(function() {
   if (gon.run) {
-    App.cable.subscriptions.create({channel: "RunChannel", run_id: gon.run.id}, {received: function(payload) {
+    const subscription = App.cable.subscriptions.create({channel: "RunChannel", run_id: gon.run.id}, {received: function(payload) {
       document.getElementById('time-since-upload').textContent = payload.time_since_upload
     }})
+    $(document).on("page:before-change turbolinks:before-visit", function() {
+      App.cable.subscriptions.remove(subscription)
+    })
   }
 })
