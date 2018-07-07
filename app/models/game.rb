dc2d1bc39d8baf0437682b949b5eab1247384896
glacials <qhiiyr@gmail.com>
2018-07-06 21:17:24 -0700

    Messily fix duplicate search terms

--- app/models/game.rb
+++ app/models/game.rb
@@ -27,7 +27,7 @@ class Game < ApplicationRecord
     term.strip!
     return Game.none if term.blank?
 
-    Game.joins(:aliases).merge(GameAlias.search_for_name(term)).uniq
+    Game.where(id: Game.joins(:aliases).merge(GameAlias.search_for_name(term)).pluck(:id).uniq)
   end
 
   def self.from_name(name)
