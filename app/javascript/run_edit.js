5deb51bcf1d2cad30607e42c5f5fb16f7f814553
glacials <qhiiyr@gmail.com>
2018-07-06 19:25:22 -0700

    Second attempt at fixing run game/category editing

--- app/javascript/run_edit.js
+++ app/javascript/run_edit.js
@@ -13,17 +13,17 @@ document.addEventListener('turbolinks:load', function() {
   gameSelect.addEventListener('change', function(event) {
     saveButton.disabled = true
     categorySelect.disabled = true
-    for(option of categorySelect.children) {
+    Array.from(categorySelect.children).forEach(function(option) {
       option.remove()
-    }
+    })
     categorySelect.appendChild(loading)
 
     fetch(`/api/v4/games/${this.value}`).then(function(response) {
       return response.json()
     }).then(function(response) {
-      for(option of categorySelect.children) {
+      Array.from(categorySelect.children).forEach(function(option) {
         option.remove()
-      }
+      })
       response.game.categories.forEach(function(category) {
         const option = document.createElement('option')
         option.value = category.id
