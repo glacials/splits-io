cae5c3377aacecdb99fef2da20d393e049189403
glacials <qhiiyr@gmail.com>
2018-07-07 23:03:14 -0700

    Fix parse controller render issues

--- app/controllers/api/webhooks/parse_controller.rb
+++ app/controllers/api/webhooks/parse_controller.rb
@@ -4,7 +4,7 @@ class Api::Webhooks::ParseController < ApplicationController
   def create
     return if @run.parsed_at.present?
     @run.parse_into_db
-    render :ok
+    head :ok
   end
 
   private
@@ -12,6 +12,6 @@ class Api::Webhooks::ParseController < ApplicationController
   def set_run
     @run = Run.find_by(s3_filename: params[:s3_filename])
   rescue ActiveRecord::RecordNotFound
-    render :not_found
+    head :not_found
   end
 end
