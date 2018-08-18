393d66a94b336961928567200400ad7c728868ea
Eugene <davygora.e@gmail.com>
2018-08-18 22:35:42 +0300

    Fix Rubocop errors in models (#432)

--- app/models/twitch_user_follow.rb
+++ app/models/twitch_user_follow.rb
@@ -1,5 +1,5 @@
 class TwitchUserFollow < ApplicationRecord
-  validates_uniqueness_of :to_user_id, scope: :from_user_id
+  validates :to_user_id, uniqueness: {scope: :from_user_id}
 
   belongs_to :from_user, class_name: 'User'
   belongs_to :to_user, class_name: 'User'
