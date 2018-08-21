af35703e56d882e966640bcdb1c886f4013ef8ee
glacials <qhiiyr@gmail.com>
2018-08-21 10:18:51 -0700

    CR comments

--- app/models/concerns/runner_user.rb
+++ app/models/concerns/runner_user.rb
@@ -6,9 +6,9 @@ module RunnerUser
   included do
     def s3_bytes_used(since: 100.years.ago)
       runs.pluck(:s3_filename).sum(0) do |s3_filename|
-        o = $s3_bucket_internal.object("splits/#{s3_filename}")
-        if o.exists? && o.last_modified > since
-          o.content_length
+        s3_run_object = $s3_bucket_internal.object("splits/#{s3_filename}")
+        if s3_run_object.exists? && s3_run_object.last_modified > since
+          s3_run_object.content_length
         else
           0
         end
