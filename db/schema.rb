cdf16382d6326406c0a730a84f25ddf73fc76cf4
glacials <qhiiyr@gmail.com>
2018-08-27 15:27:05 -0700

    Upgrade doorkeeper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_215913) do
+ActiveRecord::Schema.define(version: 2018_08_27_170856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_215913) do
     t.integer "owner_id"
     t.string "owner_type"
     t.datetime "secret_generated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.boolean "confidential", default: true, null: false
     t.index ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type"
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
