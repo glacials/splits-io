d90d1d8a57c0980b3841b34c95cb3fbb1cbd1c07
glacials <qhiiyr@gmail.com>
2018-09-16 21:17:41 -0700

    Fix a different type of S3 issue

--- app/models/concerns/runner_user.rb
+++ app/models/concerns/runner_user.rb
@@ -5,11 +5,12 @@ module RunnerUser
 
   included do
     def s3_bytes_used(since: 100.years.ago)
-      runs.pluck(:s3_filename).sum(0) do |s3_filename|
-        s3_run_object = $s3_bucket_internal.object("splits/#{s3_filename}")
-        if s3_run_object.exists? && s3_run_object.last_modified > since
-          s3_run_object.content_length
-        else
+      runs.where('created_at > ?', since).sum(0) do |run|
+        begin
+          s3_run_object = $s3_bucket_internal.object("splits/#{run.s3_filename}")
+          s3_run_object.exists? ? s3_run_object.content_length : 0
+        rescue Aws::S3::Errors::Forbidden => e
+          Rollbar.warn(e, 'Missing/forbidden S3 file while counting runs', run_id: run.id, s3_filename: run.s3_filename)
           0
         end
       end
