5527c60f5f679a02fe007585f3bd2fa2c4c32d7f
glacials <qhiiyr@gmail.com>
2018-09-26 10:53:22 -0700

    Attempt at on-box deploys

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,4 +3,5 @@ cd /app
 
 # ~/.env is created by the launch configuration's User Data. It's just a file full of "export SOME_ENV_VAR=value".
 source ~/.env
+/usr/local/bin/docker-compose --file docker-compose-production.yml down -d > /var/log/docker-compose.log 2> /var/log/docker-compose.log
 /usr/local/bin/docker-compose --file docker-compose-production.yml up -d > /var/log/docker-compose.log 2> /var/log/docker-compose.log
