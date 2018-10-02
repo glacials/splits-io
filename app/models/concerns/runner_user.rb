72ea93166119fc7cbb24d19922e21e34a08e440d
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-10-02 16:16:07 -0700

    Fixes sum depreciation notice and behavior (#454)
    `sum(0) do` was actually ignoring the 0 already and returning nil, so this fixes the behavior as well as correctly returning 0 from the function.

--- app/models/concerns/runner_user.rb
+++ app/models/concerns/runner_user.rb
@@ -5,15 +5,13 @@ module RunnerUser
 
   included do
     def s3_bytes_used(since: 100.years.ago)
-      runs.where('created_at > ?', since).sum(0) do |run|
-        begin
-          s3_run_object = $s3_bucket_internal.object("splits/#{run.s3_filename}")
-          s3_run_object.exists? ? s3_run_object.content_length : 0
-        rescue Aws::S3::Errors::Forbidden => e
-          Rollbar.warn(e, 'Missing/forbidden S3 file while counting runs', run_id: run.id, s3_filename: run.s3_filename)
-          0
-        end
-      end
+      runs.where('created_at > ?', since).sum do |run|
+        s3_run_object = $s3_bucket_internal.object("splits/#{run.s3_filename}")
+        s3_run_object.exists? ? s3_run_object.content_length : 0
+      rescue Aws::S3::Errors::Forbidden => e
+        Rollbar.warn(e, 'Missing/forbidden S3 file while counting runs', run_id: run.id, s3_filename: run.s3_filename)
+        0
+      end || 0
     end
   end
 end
