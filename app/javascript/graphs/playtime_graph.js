73d3757a8d0aec947a3a29954b341b3f4639a30f
glacials <qhiiyr@gmail.com>
2018-12-06 12:59:18 -0800

    Fix graph errors for runs with 0-1 attempts

--- app/javascript/graphs/playtime_graph.js
+++ app/javascript/graphs/playtime_graph.js
@@ -104,7 +104,7 @@ const build_playtime_graph = function(run) {
     series: [{
       name: 'Life playtime at time of PB',
       data: playtime_between_pbs,
-      regression: true,
+      regression: playtime_between_pbs >= 2,
       regressionSettings: {
         name: 'Prediction',
         type: 'logarithmic',
