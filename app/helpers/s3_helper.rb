98a91217b5f9d760f8a164c99c431673b3e407f6
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2018-12-14 22:13:22 -0800

    respect `since` parameter (#475)

--- app/helpers/s3_helper.rb
+++ app/helpers/s3_helper.rb
@@ -1,6 +1,6 @@
 module S3Helper
   def s3_bytes_used(user, since: 100.years.ago)
-    number_to_human_size(user.s3_bytes_used)
+    number_to_human_size(user.s3_bytes_used(since: since))
   rescue Aws::Waiters::Errors::UnexpectedError => e
     Rollbar.warn(e, 'Unable to fetch S3 bytes')
     '???'
