41440a34c352a69cd38c3874e1e0649e98935920
glacials <qhiiyr@gmail.com>
2019-01-21 11:57:28 -0800

    Add totals to admin, organize admin nav

--- db/schema.rb
+++ db/schema.rb
@@ -152,7 +152,7 @@ ActiveRecord::Schema.define(version: 2019_01_20_174533) do
     t.datetime "updated_at", null: false
     t.integer "owner_id"
     t.string "owner_type"
-    t.datetime "secret_generated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "secret_generated_at", default: -> { "now()" }, null: false
     t.boolean "confidential", default: true, null: false
     t.index ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type"
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
@@ -306,7 +306,7 @@ ActiveRecord::Schema.define(version: 2019_01_20_174533) do
     t.string "twitch_id", null: false
     t.string "email"
     t.string "avatar", null: false
-    t.datetime "follows_synced_at", default: "1970-01-01 00:00:00", null: false
+    t.datetime "follows_synced_at", default: "1918-08-29 06:20:14", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url", null: false
