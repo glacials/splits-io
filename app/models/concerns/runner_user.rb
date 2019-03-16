32d02073207fee66bae2093b3fb365fa76febe0b
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2019-03-15 23:27:33 -0700

    Store run filesize in db (2/2)
    backfill the actual content_length's and sum the column

--- app/models/concerns/runner_user.rb
+++ app/models/concerns/runner_user.rb
@@ -5,13 +5,7 @@ module RunnerUser
 
   included do
     def s3_bytes_used(since: 100.years.ago)
-      runs.where('created_at > ?', since).sum do |run|
-        s3_run_object = $s3_bucket_internal.object("splits/#{run.s3_filename}")
-        s3_run_object.exists? ? s3_run_object.content_length : 0
-      rescue Aws::S3::Errors::Forbidden => e
-        Rollbar.warn(e, 'Missing/forbidden S3 file while counting runs', run_id: run.id, s3_filename: run.s3_filename)
-        0
-      end || 0
+      runs.where('created_at > ?', since).sum(:filesize_bytes)
     end
   end
 end
