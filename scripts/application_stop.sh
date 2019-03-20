8584e431ee456e93cd3f743b291cb1e4425ca3cf
glacials <qhiiyr@gmail.com>
2019-03-20 16:16:30 -0700

    Fix a bad arg when stopping a container

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,5 +3,5 @@
 if [ -d /app ]
 then
   cd /app
-  /usr/local/bin/docker-compose --file docker-compose-production.yml down -d > /var/log/docker-compose.log 2> /var/log/docker-compose.log
+  /usr/local/bin/docker-compose --file docker-compose-production.yml down > /var/log/docker-compose.log 2> /var/log/docker-compose.log
 fi
