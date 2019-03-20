738062a6bb5a93f8388667bcf07fbf0e5c272964
glacials <qhiiyr@gmail.com>
2019-03-20 13:19:31 -0700

    Make deploys reuse prebuilt images

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /app
 
-# ~/.env is created by the launch configuration's User Data. It's just a file full of "export SOME_ENV_VAR=value".
 source ~/.env
+docker load < dist.tar
 /usr/local/bin/docker-compose --file docker-compose-production.yml up -d > /var/log/docker-compose.log 2> /var/log/docker-compose.log
