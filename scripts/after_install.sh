3ae9fe1bace5054ebfa9a7d3f61b0b382af4b52a
glacials <qhiiyr@gmail.com>
2019-03-20 13:45:36 -0700

    Rearrange CodeDeploy hooks

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /app
+docker load < dist.tar
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-cd /app
 
+cd /app
 source ~/.env
-docker load < dist.tar
 /usr/local/bin/docker-compose --file docker-compose-production.yml up -d > /var/log/docker-compose.log 2> /var/log/docker-compose.log
