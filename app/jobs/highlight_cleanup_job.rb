b4d91213cb18bc5678fb6f6ab95038a9effae2d9
glacials <qhiiyr@gmail.com>
2019-05-06 12:28:40 -0700

    Fix a job that doesn't know to stop

--- app/jobs/highlight_cleanup_job.rb
+++ app/jobs/highlight_cleanup_job.rb
@@ -2,6 +2,8 @@ class HighlightCleanupJob < ApplicationJob
   queue_as :highlight_cleanup
 
   def perform(highlight_suggestion)
+    return if highlight_suggestion.nil?
+
     highlight_suggestion.destroy
   end
 end
