2f9758a300f2584e820b5e8a9a1a349ea549de99
glacials <qhiiyr@gmail.com>
2019-07-30 14:14:43 -0700

    Allow reusing the attach form after an error

--- app/javascript/race_attach.js
+++ app/javascript/race_attach.js
@@ -1,6 +1,12 @@
-document.addEventListener('direct-uploads:end', () => {
+const reactivateAttachForm = () => {
   Array.from(document.querySelectorAll('form > input[type=file]')).forEach(input => {
     input.value = null
     input.disabled = false
   })
-})
+  Array.from(document.querySelectorAll('form > input[type=submit]')).forEach(input => {
+    input.disabled = false
+  })
+}
+
+document.addEventListener('direct-uploads:end', reactivateAttachForm)
+document.addEventListener('direct-uploads:error', reactivateAttachForm)
