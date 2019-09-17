a41455d32c48948315c934828ecf8e7dc06c1152
Chris Moore <moorecp@gmail.com>
2019-09-17 12:31:42 -0400

    When comparing runs, mousing-over a segment in the timeline will shift the other timelines and associated gold timelines to line up their matching segments.

--- app/javascript/timeline.js
+++ app/javascript/timeline.js
@@ -7,7 +7,18 @@ document.addEventListener('mouseover', event => {
   const run_id = segment.dataset.run_id
   const segment_number = segment.dataset.segment_number
   const query = `.segment-inspect[data-run_id='${run_id}'][data-segment_number='${segment_number}']`
+  const leftEdge = segment.offsetLeft
   document.querySelector(query).style.visibility = 'visible'
+  document.querySelectorAll(`.split[data-segment_number='${segment_number}']:not([data-run_id='${run_id}'])`).forEach((el) => {
+    const otherLeftEdge = el.offsetLeft
+    const timeline = el.closest('.timeline-background')
+    const goldTimeline = timeline.parentElement.querySelector('.gold.timeline')
+    const offset = leftEdge - otherLeftEdge
+    timeline.style.position = 'relative'
+    timeline.style.left = `${offset}px`
+    goldTimeline.style.position = 'relative'
+    goldTimeline.style.left = `${offset}px`
+  })
 })
 
 document.addEventListener('mouseout', event => {
@@ -20,4 +31,12 @@ document.addEventListener('mouseout', event => {
   const segment_number = segment.dataset.segment_number
   const query = `.segment-inspect[data-run_id='${run_id}'][data-segment_number='${segment_number}']`
   document.querySelector(query).style.visibility = 'hidden'
+  document.querySelectorAll(`.split[data-segment_number='${segment_number}']:not([data-run_id='${run_id}'])`).forEach((el) => {
+    const timeline = el.closest('.timeline-background')
+    const goldTimeline = timeline.parentElement.querySelector('.gold.timeline')
+    timeline.style.position = ""
+    timeline.style.left = ""
+    goldTimeline.style.position = ""
+    goldTimeline.style.left = ""
+  })
 })
