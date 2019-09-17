92e70d121a42f29e30b4b03442531f5045a753cc
Chris Moore <moorecp@gmail.com>
2019-09-16 23:41:34 -0400

    Made the fields on Video non-nullable since the record makes no sense without them

--- db/migrate/20190915141533_create_videos.rb
+++ db/migrate/20190915141533_create_videos.rb
@@ -1,8 +1,8 @@
 class CreateVideos < ActiveRecord::Migration[6.0]
   def change
     create_table :videos do |t|
-      t.references :run, index: true
-      t.string :url
+      t.references :run, index: true, null: false
+      t.string :url, null: false
 
       t.timestamps
     end
