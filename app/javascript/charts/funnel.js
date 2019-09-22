c4dc66a89b8e3dd9929ca10426500a6e91012835
Chris Moore <moorecp@gmail.com>
2019-09-21 20:52:11 -0400

    Keep funnel charts from overlapping when there are multiple runs.

--- app/javascript/charts/funnel.js
+++ app/javascript/charts/funnel.js
@@ -27,27 +27,26 @@ const buildFunnelChart = function(runs, options = {}) {
           enabled: true,
           format: '<b>{point.name}</b> ({point.y:,.0f})',
           softConnector: true
-        },
-        center: ['40%', '50%'],
-        neckWidth: '30%',
-        neckHeight: '25%',
-        width: '80%'
+        }
       }
     },
     legend: {
         enabled: false
     },
-    series: runs.map((run) => (
+    series: runs.map((run, i) => (
       {
-        name: 'Runs reached split',
+        center: [`${(100/(runs.length+1))*(i+1)}%`, '50%'],
         data: run.segments.map((segment) => (
           [segment.name, segment.histories.length]
         )),
+        name: 'Runs reached split',
+        neckWidth: runs.length === 1 ? '30%' : `${30/runs.length*0.8}%`,
         tooltip: {
           pointFormatter: function() {
             return `<span style="color:{this.point.color}">\u25CF</span> ${this.series.name}: ${this.y} | ${(this.y / run.attempts * 100).toFixed(1)}%`
           }
-        }
+        },
+        width: runs.length === 1 ? '80%' : `${100/(runs.length+1)*0.8}%`
       }
     ))
   }))
