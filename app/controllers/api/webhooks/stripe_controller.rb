a7f4159a82011bea79b04382a32c969e994eed92
glacials <qhiiyr@gmail.com>
2019-09-23 15:12:55 -0700

    Fix ending subscriptions

--- app/controllers/api/webhooks/stripe_controller.rb
+++ app/controllers/api/webhooks/stripe_controller.rb
@@ -1,11 +1,10 @@
 class Api::Webhooks::StripeController < ApplicationController
   before_action :set_event, only: %i[create]
-  before_action :set_subscriptions, only: %i[create]
 
   def create
     case @event.type
     when 'checkout.session.completed'
-      @subscriptions.cancel_all!
+      Subscription.where(user_id: @event.data.object.client_reference_id).cancel_all!
       Subscription.create(
         user_id: @event.data.object.client_reference_id,
         stripe_session_id: @event.data.object.id,
@@ -15,7 +14,8 @@ class Api::Webhooks::StripeController < ApplicationController
         stripe_customer_id: @event.data.object.customer,
       )
     when 'customer.subscription.deleted'
-      @subscriptions.update_all(ended_at: Time.now.utc)
+      @subscription = Subscription.where(stripe_subscription_id: @event.data.object.id)
+      @subscription.update(ended_at: Time.now.utc)
     end
   end
 
@@ -27,9 +27,5 @@ class Api::Webhooks::StripeController < ApplicationController
       request.headers['HTTP_STRIPE_SIGNATURE'],
       ENV['STRIPE_WEBHOOK_SECRET'],
     )
-  end
-
-  def set_subscriptions
-    @subscriptions = Subscription.where(user_id: @event.data.object.client_reference_id)
   end
 end
