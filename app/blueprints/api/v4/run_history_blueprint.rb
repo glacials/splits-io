702c9b023ababfe3198a469b9a3cc1a06b8889d1
Chris Moore <moorecp@gmail.com>
2019-09-24 21:32:47 -0400

    Don't send fake started_at / ended_at dates

--- app/blueprints/api/v4/run_history_blueprint.rb
+++ app/blueprints/api/v4/run_history_blueprint.rb
@@ -1,21 +1,3 @@
 class Api::V4::RunHistoryBlueprint < Blueprinter::Base
   fields :id, :attempt_number, :realtime_duration_ms, :gametime_duration_ms, :started_at, :ended_at
-
-  field(:started_at) do |history, _|
-    if history.started_at
-      history.started_at
-    else
-      DateTime.new(2013, 11, 13, 0, 0, 0)
-    end
-  end
-
-  field(:ended_at) do |history, _|
-    if history.ended_at
-      history.ended_at
-    elsif history.realtime_duration_ms
-      DateTime.new(2013, 11, 13, 0, 0, 0) + (history.realtime_duration_ms / 1000.0).seconds
-    else
-      DateTime.new(2013, 11, 13, 0, 0, 0)
-    end
-  end
 end
