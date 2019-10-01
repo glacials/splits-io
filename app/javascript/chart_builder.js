28d2ee016377d62eb67fb60858225ac5138ec08c
Chris Moore <moorecp@gmail.com>
2019-10-01 10:52:05 -0400

    Temporarily adding additional charts for comparison

--- app/javascript/chart_builder.js
+++ app/javascript/chart_builder.js
@@ -5,6 +5,8 @@ import {buildPlaytimeChart} from "charts/playtime.js"
 import {buildResetChart} from "charts/reset.js"
 import {buildBoxPlot} from "charts/box_plot.js"
 import {buildFunnelChart} from "charts/funnel.js"
+import {buildRunProgressLineChart} from "charts/run_progress_line.js"
+import {buildRunProgressVariwideChart} from "charts/run_progress_variwide.js"
 import {chartOptions} from "consts.js"
 import {createSpinner} from 'spinner.js'
 import Highcharts from 'highcharts'
@@ -89,6 +91,8 @@ document.addEventListener('turbolinks:load', function() {
       buildSegmentChart(runs, chartOptions)
       buildResetChart(runs, chartOptions)
       buildFunnelChart(runs, chartOptions)
+      buildRunProgressLineChart(runs, chartOptions)
+      buildRunProgressVariwideChart(runs, chartOptions)
       buildPlaytimeChart(runs, chartOptions)
 
       runs[0].segments.filter(segment => !segment.skipped).forEach((segment, i) => {
---
+++ app/javascript/charts/run_progress_variwide.js
@@ -0,0 +1,61 @@
+import Highcharts from 'highcharts'
+import Exporting from 'highcharts/modules/exporting'
+import Variwide from 'highcharts/modules/variwide'
+Exporting(Highcharts)
+Variwide(Highcharts)
+
+const defaults = require('deep-defaults')
+
+const buildRunProgressVariwideChart = function(runs, options = {}) {
+  if (document.getElementById('run-progress-variwide-chart') === null) {
+    return
+  }
+
+  Highcharts.chart('run-progress-variwide-chart', defaults(_.clone(options), {
+    exporting: {
+      chartOptions: {
+        legend: {
+          enabled: false
+        }
+      }
+    },
+    chart: {type: 'variwide'},
+    title: {text: 'Run Progress'},
+    legend: {
+        enabled: runs.length > 1
+    },
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      max: 100,
+      min: 0,
+      title: {
+        text: 'Percent of Runs Reached Split'
+      }
+    },
+    tooltip: {
+      pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: {point.y}%',
+    },
+    series: runs.map((run, i) => (
+      {
+        data: run.segments.map((segment) => (
+          [segment.name, parseFloat((segment.histories.length / run.attempts * 100).toFixed(1)), 20]
+        )),
+        name: (run.runners[0] || {name: '???'}).name,
+        colorByPoint: runs.length == 1,
+        // dataLabels: {
+        //   formatter: function() { return moment.duration(this.y).format('H:mm:ss') }
+        // },
+
+        // tooltip: {
+        //   pointFormatter: function() {
+        //     return `<span style="color:${this.series.color}">\u25CF</span> ${this.series.name}: ${this.y}%`
+        //   }
+        // }
+      }
+    ))
+  }))
+}
+
+export {buildRunProgressVariwideChart}
