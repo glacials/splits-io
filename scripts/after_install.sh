a5dba5cd797e00c04799e4b516db15279c62a002
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2019-10-08 17:38:33 -0700

    Redo pipeline stuff for faster deploys (#618)

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 cd /app
-docker load < dist.tar
-rm dist.tar
+export REPOSITORY_URI="767531804574.dkr.ecr.us-east-1.amazonaws.com/splitsio-prod-builder"
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker pull $REPOSITORY_URI:latest
+source ~/.env
+/usr/local/bin/docker-compose -f docker-compose-production.yml build web
