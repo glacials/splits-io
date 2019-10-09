857c43e5ff638a63fc42b7f61d1895aae1e9cd32
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2019-10-08 21:16:42 -0700

    Fix for multiple pipelines

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,9 @@
 
 cd /app
 export REPOSITORY_URI="767531804574.dkr.ecr.us-east-1.amazonaws.com/splitsio-prod-builder"
+export REVISION=$(cat revision.txt)
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker pull $REPOSITORY_URI:latest
+docker pull $REPOSITORY_URI:$REVISION
+docker tag $REPOSITORY_URI:$REVISION $REPOSITORY_URI:latest
 source ~/.env
 /usr/local/bin/docker-compose -f docker-compose-production.yml build web
