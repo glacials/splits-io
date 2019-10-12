b6b45ae4b8d0e4271ad7a665a16f8ab077c8fcf2
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2019-10-12 14:38:15 -0700

    scope migration to only runs that have never been parsed

--- db/migrate/20191006212943_cleanup_runs_with_no_files.rb
+++ db/migrate/20191006212943_cleanup_runs_with_no_files.rb
@@ -1,6 +1,6 @@
 class CleanupRunsWithNoFiles < ActiveRecord::Migration[6.0]
   def up
-    Run.find_each do |run|
+    Run.where(parsed_at: nil).find_each do |run|
       file = $s3_bucket_internal.object("splits/#{run.s3_filename}")
       next if file.exists?
 
