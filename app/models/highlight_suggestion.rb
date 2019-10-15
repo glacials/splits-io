1cf28767c6be8390c8c2756a6006e90da882637f
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2019-10-15 14:16:04 -0700

    Guard against twitch highlight errors (#631)

--- app/models/highlight_suggestion.rb
+++ app/models/highlight_suggestion.rb
@@ -27,30 +27,34 @@ class HighlightSuggestion < ApplicationRecord
 
         video_start    = DateTime.parse(video['created_at'])
         video_end      = video_start + hours + minutes + seconds
-        video_duration = (video_end - video_start) * 1.day # subtracting DateTimes gives fractional days; we want seconds
+        # subtracting DateTimes gives fractional days; we want seconds
+        video_duration = (video_end - video_start) * 1.day
+        next unless video_start - 30.seconds < pb.started_at && video_end + 30.seconds > pb.ended_at
 
-        if video_start - 30.seconds < pb.started_at && video_end + 30.seconds > pb.ended_at
-          video_time_at_pb_start = pb.started_at - video_start
-          video_time_at_pb_end   = video_time_at_pb_start + (pb.duration(Run::REAL).to_ms / 1000)
+        video_time_at_pb_start = pb.started_at - video_start
+        video_time_at_pb_end   = video_time_at_pb_start + (pb.duration(Run::REAL).to_ms / 1000)
 
-          highlight_suggestion = create(
-            run: run,
-            url: URI.parse("https://www.twitch.tv/#{run.user.twitch.name}/manager/highlighter/#{video['id']}").tap do |uri|
-              uri.query = {
-                start: [0, (video_time_at_pb_start - 10.seconds).to_i].max,
-                end:   [video_duration.to_i, (video_time_at_pb_end + 10.seconds).to_i].min,
-                title: "PB: #{run.game} #{run.category} in #{run.duration(run.default_timing).format}"
-              }.to_query
-            end
-          )
-          # 60 days is life of archives for Partners / Twitch Prime members
-          if highlight_suggestion.persisted?
-            HighlightCleanupJob.set(wait_until: video_start + 60.days).perform_later(highlight_suggestion)
+        highlight_suggestion = create(
+          run: run,
+          url: URI.parse("https://www.twitch.tv/#{run.user.twitch.name}/manager/highlighter/#{video['id']}").tap do |uri|
+            uri.query = {
+              start: [0, (video_time_at_pb_start - 10.seconds).to_i].max,
+              end:   [video_duration.to_i, (video_time_at_pb_end + 10.seconds).to_i].min,
+              title: "PB: #{run.game} #{run.category} in #{run.duration(run.default_timing).format}"
+            }.to_query
           end
-          return highlight_suggestion
+        )
+        if highlight_suggestion.persisted?
+          # 60 days is life of archives for Partners / Twitch Prime members
+          HighlightCleanupJob.set(wait_until: video_start + 60.days).perform_later(highlight_suggestion)
         end
+        return highlight_suggestion
       end
 
+      nil
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.error([e.message, *e.backtrace].join($RS))
+      Rollbar.error(e, response: e.response)
       nil
     end
   end
