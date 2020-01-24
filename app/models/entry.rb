8477c2c1581e990f197a6fc47eddca3fc5fa91e2
glacials <qhiiyr@gmail.com>
2020-01-24 12:29:02 -0800

    Make entries table more efficient

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,11 @@ class Entry < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   belongs_to :run, dependent: :destroy, optional: true
 
+  # coentries can be called instead of entry.race.entries to avoid needing to join on races. It can also be used as a
+  # join model to e.g. discover number of 1st/2nd/3rd places by user more cheaply as a Rails-friendly query. This
+  # relation INCLUDES "myself", e.g. entry.coentries will include entry in returned values.
+  has_many :coentries, source: :entries, class_name: 'Entry', primary_key: 'race_id', foreign_key: 'race_id'
+
   validates_with EntryValidator
   # Validators are not called before destroy's, so manually hook and prevent if race is started
   before_destroy :validate_destroy
@@ -19,6 +24,14 @@ class Entry < ApplicationRecord
 
   scope :unfinished, ->  { where(finished_at: nil)  }
   scope :unforfeited, -> { where(forfeited_at: nil) }
+
+  # Filters entries to only those which came in first place in their respective races. This is a far less expensive way
+  # than using Entry#place.
+  scope :first_place, -> do
+    joins('LEFT JOIN entries coentries ON entries.race_id = coentries.race_id AND entries.finished_at > coentries.finished_at')
+      .where('coentries.finished_at IS NULL')
+      .where('entries.finished_at IS NOT NULL')
+  end
 
   # Returns this race's entry for the given user, not including ghosts.
   def self.find_for(user)
@@ -41,6 +54,7 @@ class Entry < ApplicationRecord
     finished? || forfeited?
   end
 
+  # TODO: This is expensive for how often it gets called (see app/views/race/_entries_table.slim); can we improve?
   def place
     return nil unless finished?
 
