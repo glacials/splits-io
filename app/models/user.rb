0dc9aac1c4377d2ee11f225d3b4ae621210d5f76
glacials <qhiiyr@gmail.com>
2020-01-24 16:41:41 -0800

    Fix a Gold vs. Patreon bug
    Fix a bug where speedrun.com auto-submit was not available to patrons
    paying the equivalent of Gold via Patreon. Added a bunch of comments for
    each feature flag on the user explaining the reasoning for whether or
    not they have each feature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -102,44 +102,79 @@ class User < ApplicationRecord
     name || 'somebody'
   end
 
+  # Predictions used to be a lower-tier paid Patreon feature, so they're available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Continued patrons who used to have them at the lower price point (grandfathered)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_predictions?
-    patron?(tier: 2) || subscriptions.tier1.active.any? || admin?
+    patron?(tier: 2, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier1.active.any? || admin?
   end
 
+  # Redirectors used to be a lower-tier paid Patreon feature, so they're available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Continued patrons who used to have them at the lower price point (grandfathered)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_redirectors?
-    patron?(tier: 3) || subscriptions.tier2.active.any? || admin?
+    patron?(tier: 3, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier2.active.any? || admin?
   end
 
+  # Advanced comparisons used to be free, so they're available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Early patrons (grandfathered as a thank-you)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_advanced_comparisons?
-    patron?(tier: 3) || subscriptions.tier2.active.any? || admin?
+    patron?(tier: 3, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier2.active.any? || admin?
   end
 
+  # Sum-of-best leaderboards used to be free, so they're available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Early patrons (grandfathered as a thank-you)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_sum_of_best_leaderboards?
-    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || subscriptions.tier1.active.any? || admin?
+    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier1.active.any? || admin?
   end
 
+  # Hiding used to be free, so it's available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Early patrons (grandfathered as a thank-you)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_hiding?
-    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || subscriptions.tier1.active.any? || admin?
+    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier1.active.any? || admin?
   end
 
+  # Advanced video used to be free, so it's available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Early patrons (grandfathered as a thank-you)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_advanced_video?
-    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || subscriptions.tier1.active.any? || admin?
+    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier1.active.any? || admin?
   end
 
+  # Auto-highlight used to be free, so it's available to:
+  # 1. Gold users (the standard way of obtaining)
+  # 2. Early patrons (grandfathered as a thank-you)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_autohighlight?
-    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || subscriptions.tier1.active.any? || admin?
+    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier1.active.any? || admin?
   end
 
+  # Advanced analytics used to be lower-tier paid Patreon features, so they're available to:
+  # 1. Gold users (now the standard way of obtaining)
+  # 2. Continued patrons who used to have them at the lower price point (grandfathered)
+  # 3. Anyone paying at least the price of Gold via Patreon
   def has_advanced_analytics?
-    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || subscriptions.tier1.active.any? || admin?
+    patron?(tier: 1, before: STRIPE_MIGRATION_DATE) || patron?(tier: 4) || subscriptions.tier1.active.any? || admin?
   end
 
+  # Speedrun.com auto-submit is the first post-paywall-move paid feature, so is only available to Gold users and
+  # those who are paying at least the equivalent at Patreon.
   def has_srdc_submit?
-    subscriptions.tier3.active.any? || admin?
+    patron?(tier: 4) || subscriptions.tier3.active.any? || admin?
   end
 
   # patron? returns something truthy if the user has been a patron at or above the given tier since the given `before`
-  # time. Not passing a `before` is the same as not caring how long the user has been a patron.
+  # time. Not passing a `before` is the same as not caring how long the user has been a patron. Tier 4 is an imaginary
+  # tier equal to the price of Gold.
   def patron?(tier: 0, before: Time.now.utc)
     return false if patreon&.pledge_created_at.nil? || patreon.pledge_created_at > before
 
@@ -152,6 +187,8 @@ class User < ApplicationRecord
       patreon.pledge_cents >= 400
     when 3
       patreon.pledge_cents >= 600
+    when 4
+      patreon.pledge_cents >= 900
     end
   end
 
