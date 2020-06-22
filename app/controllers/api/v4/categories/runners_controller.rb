9913e81288a1d20e4b0efbddf7380309ec0311cf
Chris Moore <chris@monoclesoftware.com>
2020-06-22 18:32:34 -0400

    Change the categories/runners endpoint to return unique runners (#700)
    Co-authored-by: Chris Moore <moorecp@gmail.com>

--- app/controllers/api/v4/categories/runners_controller.rb
+++ app/controllers/api/v4/categories/runners_controller.rb
@@ -2,6 +2,6 @@ class Api::V4::Categories::RunnersController < Api::V4::ApplicationController
   before_action :set_category, only: [:index]
 
   def index
-    render json: Api::V4::UserBlueprint.render(@category.users, root: :runners)
+    render json: Api::V4::UserBlueprint.render(@category.runners, root: :runners)
   end
 end
