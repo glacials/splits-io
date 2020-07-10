371ca659a5c24670f4bf06dde69d6d3cb5f2a30c
Chris Moore <moorecp@gmail.com>
2020-07-10 12:55:42 -0400

    Removed some semicolons

--- app/javascript/youtube_timestamps.js
+++ app/javascript/youtube_timestamps.js
@@ -16,24 +16,24 @@ document.addEventListener('click', event => {
   require('moment-duration-format')(moment)
 
   const offsetInMilliseconds = (document.getElementById('video_start_offset_seconds').value || 0) * 1000
-  const textarea = document.getElementById('youtube_timestamps');
-  const segments = JSON.parse(textarea.dataset.segments);
+  const textarea = document.getElementById('youtube_timestamps')
+  const segments = JSON.parse(textarea.dataset.segments)
   const offsets = segments.map((segment) => {
     const duration = segment.start_ms + offsetInMilliseconds
-    const format = duration < 3600000 ? 'm:ss' : 'H:mm:ss';
-    const segmentStart = moment.duration(duration).format(format, {trim: false});
+    const format = duration < 3600000 ? 'm:ss' : 'H:mm:ss'
+    const segmentStart = moment.duration(duration).format(format, {trim: false})
     return `${segmentStart} ${htmlDecode(segment.name)}`
   })
   if (offsetInMilliseconds > 0) {
-    offsets.unshift("0:00 Start of Video");
+    offsets.unshift("0:00 Start of Video")
   }
   textarea.value = offsets.join("\n")
-  textarea.select();
-  document.execCommand("copy");
+  textarea.select()
+  document.execCommand("copy")
   return false
 })
 
 function htmlDecode(string) {
-  const parser = new DOMParser().parseFromString(string, "text/html");
+  const parser = new DOMParser().parseFromString(string, "text/html")
   return parser.documentElement.textContent
 }
