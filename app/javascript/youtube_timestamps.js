d930378843a707b1230afc8dc4aba0ee803c0733
Chris Moore <moorecp@gmail.com>
2020-07-14 11:04:56 -0400

    Fix an issue where skipped segments would not set the YouTube timestamps correctly.

--- app/javascript/youtube_timestamps.js
+++ app/javascript/youtube_timestamps.js
@@ -18,8 +18,11 @@ document.addEventListener('click', event => {
   const offsetInMilliseconds = (document.getElementById('video_start_offset_seconds').value || 0) * 1000
   const textarea = document.getElementById('youtube_timestamps')
   const segments = JSON.parse(textarea.dataset.segments)
+  let previous_segment_end = undefined
   const offsets = segments.map((segment) => {
-    const duration = segment.start_ms + offsetInMilliseconds
+    const start_ms = (segment.start_ms === 0 && previous_segment_end !== undefined) ? previous_segment_end : segment.start_ms
+    const duration = start_ms + offsetInMilliseconds
+    previous_segment_end = segment.end_ms
     const format = duration < 3600000 ? 'm:ss' : 'H:mm:ss'
     const segmentStart = moment.duration(duration).format(format, {trim: false})
     return `${segmentStart} ${htmlDecode(segment.name)}`
