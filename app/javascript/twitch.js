12b0648103035ad0b6f2cc21cc3fe68642556507
Chris Moore <chris@monoclesoftware.com>
2020-07-15 16:44:49 -0400

    Fix Twitch Player Issues (#708)
    Co-authored-by: Chris Moore <moorecp@gmail.com>
    Co-authored-by: Ben Carlsson <qhiiyr@gmail.com>

--- app/javascript/twitch.js
+++ app/javascript/twitch.js
@@ -7,28 +7,33 @@ document.addEventListener('turbolinks:load', function() {
     }
     el.dataset.loaded = '1'
 
-    const player = new Twitch.Player(el.id, {
-      autoplay: false,
-      video: el.dataset.video_id,
-      muted: true,
-      height: '100%',
-      width: '100%'
-    })
-
-    const startOffset = parseInt(el.dataset.start_offset)
-    document.addEventListener('click', event => {
-      const segment = event.target.closest('.split')
-      if (segment === null) {
-        return
-      }
-      player.seek(parseInt(segment.dataset.start_ms / 1000) + startOffset)
-      setTimeout(() => player.play(), 10) // Running this instantly after seek causes the player to hang
-    })
-
-    const ticker = document.getElementById(`video-progress-line-${el.dataset.run_id}`)
-    async.forever(next => {
-      ticker.style.margin = `0 0 0 ${Math.max(player.getCurrentTime() - startOffset, 0) / (gon.scale_to / 1000) * 100}%`
-      setTimeout(() => next(), 100)
-    })
+    setTimeout(() => attachTwitch(el), 1)
   })
 })
+
+function attachTwitch(el) {
+  const player = new Twitch.Player(el.id, {
+    autoplay: false,
+    video: el.dataset.video_id,
+    muted: true,
+    height: '100%',
+    width: '100%'
+  })
+
+  const startOffset = parseInt(el.dataset.start_offset)
+  document.addEventListener('click', event => {
+    const segment = event.target.closest('.split')
+    if (segment === null) {
+      return
+    }
+
+    player.seek(parseInt(segment.dataset.start_ms / 1000 + startOffset))
+    setTimeout(() => player.play(), 10) // Running this instantly after seek causes the player to hang
+  })
+
+  const ticker = document.getElementById(`video-progress-line-${el.dataset.run_id}`)
+  async.forever(next => {
+    ticker.style.margin = `0 0 0 ${Math.max(player.getCurrentTime() - startOffset, 0) / (gon.scale_to / 1000) * 100}%`
+    setTimeout(() => next(), 100)
+  })
+}
