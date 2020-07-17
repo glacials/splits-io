38ff6f02c52c6219879c9821cdbb823a710e3cf6
Chris Moore <chris@monoclesoftware.com>
2020-07-17 13:17:58 -0400

    Address YouTube and Turbolinks issues (#711)
    * Fix the YouTube JS such that we no longer need to poll to deal with Turbolinks issues and coming to a page with a video without a full reload actually works for things like clicking on segments.
    
    * Collapse setupYouTubeVideos into onYouTubeIframeAPIReady
    
    * Prevent some YouTube functions from polluting the global scope.
    
    Co-authored-by: Chris Moore <moorecp@gmail.com>

--- app/javascript/youtube.js
+++ app/javascript/youtube.js
@@ -1,10 +1,6 @@
 const async = require('async')
 
 window.onYouTubeIframeAPIReady = () => {
-  return true
-}
-
-function setupYouTubeVideos() {
   Array.from(document.querySelectorAll('[data-youtube]')).forEach(el => {
     if (el.dataset.loaded === '1') {
       return
@@ -14,40 +10,69 @@ function setupYouTubeVideos() {
     const player = new YT.Player(el.id, {
       height: '100%',
       width: '100%',
-      videoId: el.dataset.video_id
-    });
-
-    const startOffset = parseInt(el.dataset.start_offset)
-    document.addEventListener('click', event => {
-      const segment = event.target.closest('.split');
-      if (segment === null) {
-        return
+      videoId: el.dataset.video_id,
+      events: {
+        'onReady': onYoutubePlayerReady
       }
-
-      player.seekTo(parseInt(segment.dataset.start_ms / 1000) + startOffset)
-      setTimeout(function() { player.playVideo() }, 10) // Running this instantly after seek causes the player to hang
-    });
-
-    const ticker = document.getElementById(`video-progress-line-${el.dataset.run_id}`)
-    async.forever(next => {
-      try {
-        ticker.style.margin = `0 0 0 ${Math.max(player.getCurrentTime() - startOffset, 0) / (gon.scale_to / 1000) * 100}%`
-      } catch {}
-      setTimeout(() => next(), 100)
     })
+  })
+  return true
+}
+
+const onYoutubePlayerReady = (event) => {
+  const player = event.target
+  const el = player.getIframe()
+
+  const startOffset = parseInt(el.dataset.start_offset)
+  document.addEventListener('click', clickEvent => {
+    const segment = clickEvent.target.closest('.split');
+    if (segment === null) {
+      return
+    }
+
+    player.seekTo(parseInt(segment.dataset.start_ms / 1000) + startOffset)
+    setTimeout(function() { player.playVideo() }, 10) // Running this instantly after seek causes the player to hang
+  });
+
+  const ticker = document.getElementById(`video-progress-line-${el.dataset.run_id}`)
+  async.forever(next => {
+    try {
+      ticker.style.margin = `0 0 0 ${Math.max(player.getCurrentTime() - startOffset, 0) / (gon.scale_to / 1000) * 100}%`
+    } catch {}
+    setTimeout(() => next(), 100)
   })
 }
 
-// TODO: Is there a way we can deal with YouTube's async loading
-// and Turbolinks without polling?
-function waitForYouTubeAPI() {
-  if (window.YT !== undefined && typeof(window.YT.Player) === "function") {
-    setupYouTubeVideos()
-    return true
+const loadYouTubeJSIfNeeded = () => {
+  if (document.querySelectorAll('[data-youtube]').length === 0) {
+    return
   }
-  setTimeout(waitForYouTubeAPI, 100)
+
+  if (window.YT) {
+    // The YouTube API has been loaded once, but Turbolinks and YouTube don't get along, so let's let YouTube do its thing again
+    window.YT = undefined
+  }
+  let tag = document.createElement('script')
+  tag.setAttribute('id', 'youtube-async-script')
+
+  tag.src = "https://www.youtube.com/iframe_api"
+  let firstScriptTag = document.body.getElementsByTagName('script')[0]
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+
+  if (window.YT !== undefined && typeof(window.YT.Player) === "function") {
+    onYouTubeIframeAPIReady()
+  }
+  document.addEventListener("turbolinks:before-cache", function() {
+    // Remove the YouTube scripts from the DOM so we don't get 87 copies of it over time
+    const ytElement = document.getElementById('www-widgetapi-script')
+    if (ytElement) {
+      ytElement.remove()
+    }
+    document.getElementById('youtube-async-script').remove();
+    document.removeEventListener("turbolinks:before-cache", arguments.callee)
+  })
 }
 
 document.addEventListener('turbolinks:load', function() {
-  waitForYouTubeAPI()
+  loadYouTubeJSIfNeeded()
 })
