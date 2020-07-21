409684d36f2e657c61823c304b7eb8efea98b7c8
Michael Berk <moshe.berk@icloud.com>
2020-07-21 15:55:30 -0500

    Added optional parameter to API that only returns categories with at least one run. - Updated API documentation to reflect this change

--- app/controllers/api/v4/games/categories_controller.rb
+++ app/controllers/api/v4/games/categories_controller.rb
@@ -2,6 +2,10 @@ class Api::V4::Games::CategoriesController < Api::V4::ApplicationController
   before_action :set_game, only: [:index]
 
   def index
-    render json: Api::V4::CategoryBlueprint.render(@game.categories, root: :categories)
+	if params[:nonempty] == "true"
+    	render json: Api::V4::CategoryBlueprint.render(@game.nonempty_categories, root: :categories)
+    else 
+    	render json: Api::V4::CategoryBlueprint.render(@game.categories, root: :categories)
+    end
   end
 end
