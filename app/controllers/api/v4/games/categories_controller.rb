0658742417b7920424dd4cee7dc638734d1601b8
Michael Berk <moshe.berk@icloud.com>
2020-07-26 10:40:38 -0500

    Changed parameter from nonempty=true to filter=nonempty

--- app/controllers/api/v4/games/categories_controller.rb
+++ app/controllers/api/v4/games/categories_controller.rb
@@ -2,7 +2,7 @@ class Api::V4::Games::CategoriesController < Api::V4::ApplicationController
   before_action :set_game, only: [:index]
 
   def index
-	if params[:nonempty] == "true"
+	if params[:filter] == "nonempty"
     	render json: Api::V4::CategoryBlueprint.render(@game.categories.nonempty, root: :categories)
     else 
     	render json: Api::V4::CategoryBlueprint.render(@game.categories, root: :categories)
