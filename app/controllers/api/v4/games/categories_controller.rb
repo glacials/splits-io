2a64d23e6983dc589c85dc7dd87c4df35ec9cca1
glacials <qhiiyr@gmail.com>
2020-07-27 09:55:10 -0700

    Small styling + docs changes

--- app/controllers/api/v4/games/categories_controller.rb
+++ app/controllers/api/v4/games/categories_controller.rb
@@ -2,10 +2,10 @@ class Api::V4::Games::CategoriesController < Api::V4::ApplicationController
   before_action :set_game, only: [:index]
 
   def index
-	if params[:filter] == "nonempty"
-    	render json: Api::V4::CategoryBlueprint.render(@game.categories.nonempty, root: :categories)
+    if params[:filter] == 'nonempty'
+      render json: Api::V4::CategoryBlueprint.render(@game.categories.nonempty, root: :categories)
     else 
-    	render json: Api::V4::CategoryBlueprint.render(@game.categories, root: :categories)
+      render json: Api::V4::CategoryBlueprint.render(@game.categories, root: :categories)
     end
   end
 end
