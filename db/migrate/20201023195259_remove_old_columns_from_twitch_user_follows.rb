675789192811ab9015f46ed6e078089f2f0943b3
glacials <qhiiyr@gmail.com>
2020-10-23 14:07:11 -0700

    Revert "Remove now-unused TwitchUserFollow columns"
    This reverts commit abf76e916ed2592619e0cafb48ca4e64d08bdd46.

--- db/migrate/20201023195259_remove_old_columns_from_twitch_user_follows.rb
+++
@@ -1,8 +0,0 @@
-class RemoveOldColumnsFromTwitchUserFollows < ActiveRecord::Migration[6.0]
-  def change
-    safety_assured do
-      remove_column :twitch_user_follows, :from_user_id
-      remove_column :twitch_user_follows, :to_user_id
-    end
-  end
-end
