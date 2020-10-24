bc4425800246f7e3aa420b421090a848b84f3bae
glacials <qhiiyr@gmail.com>
2020-10-23 22:32:49 -0700

    Temporarily unignore columns that are needed for migration

--- app/models/twitch_user_follow.rb
+++ app/models/twitch_user_follow.rb
@@ -1,8 +1,8 @@
 class TwitchUserFollow < ApplicationRecord
-  self.ignored_columns = ['from_user_id', 'to_user_id']
+  #self.ignored_columns = ['from_user_id', 'to_user_id']
 
-  validates :to_twitch_user_id, uniqueness: {scope: :from_twitch_user_id}
+  validates :to_twitch_user_id, uniqueness: { scope: :from_twitch_user_id }
 
-  belongs_to :from_twitch_user, class_name: 'TwitchUser'
-  belongs_to :to_twitch_user,   class_name: 'TwitchUser'
+  belongs_to :from_twitch_user, class_name: "TwitchUser"
+  belongs_to :to_twitch_user, class_name: "TwitchUser"
 end
