98e3f387303839964c58040a48c7e48762cd8705
glacials <qhiiyr@gmail.com>
2020-10-24 09:53:50 -0700

    Unignore columns

--- app/models/twitch_user_follow.rb
+++ app/models/twitch_user_follow.rb
@@ -1,6 +1,4 @@
 class TwitchUserFollow < ApplicationRecord
-  self.ignored_columns = ["from_user_id", "to_user_id"]
-
   validates :to_twitch_user_id, uniqueness: { scope: :from_twitch_user_id }
 
   belongs_to :from_twitch_user, class_name: "TwitchUser"
