94534a1ac8fa0398236fb3f5b9c516bb5faeee17
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2020-11-15 14:49:44 -0800

    Fix bug when visiting page with count up/down before visiting race page
    also fixes warning generated from moment for non-iso string passed to constructor

--- app/javascript/vue/SpeedRunsLiveRace.vue
+++ app/javascript/vue/SpeedRunsLiveRace.vue
@@ -2,7 +2,7 @@
   <a class="list-group-item list-group-item-action flex-column align-items-start bg-transparent text-light" :href="`http://www.speedrunslive.com/race/?id=${value.id}`">
     <div class="d-flex w-100 justify-content-between">
       <h5>{{value.game.name}}</h5>
-      <h5 v-if="value.statetext === 'In Progress'" :data-abstime="new Date(value.time * 1000)" data-abstime-format="HH:mm:ss" />
+      <h5 v-if="value.statetext === 'In Progress'" :data-abstime="new Date(value.time * 1000).toISOString()" data-abstime-format="HH:mm:ss" />
       <h5 v-else-if="value.statetext === 'Entry Open'" class="text-success">Open</h5>
       <h5 v-else>{{value.statetext}}</h5>
     </div>
