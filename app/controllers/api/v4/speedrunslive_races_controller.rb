01491628a20fa2f166f531c065a900190366ecd7
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2021-08-16 13:23:41 -0700

    exclude timesync and SRL endpoints from tracking (#780)

--- app/controllers/api/v4/speedrunslive_races_controller.rb
+++ app/controllers/api/v4/speedrunslive_races_controller.rb
@@ -1,4 +1,6 @@
 class Api::V4::SpeedrunsliveRacesController < Api::V4::ApplicationController
+  skip_before_action :track
+
   def index
     Rails.cache.fetch('srl_races', expires_in: 1.minute) do
       render json: SpeedRunsLive::Race.all
