4a7c58187e5c6ffd5e51fe870fca2451baff7f9b
glacials <ben@twos.dev>
2024-10-10 18:00:39 -0700

    css kinda loading, just not 3rd party

--- dist/application.js
+++ dist/application.js
@@ -23,21 +23,38 @@ eval("// Entry point for the build script in your package.json\n\n\n//# sourceUR
 /*!*************************************************!*\
   !*** ./app/assets/stylesheets/application.sass ***!
   \*************************************************/
-/***/ (function() {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nUndefined mixin.\\n   ╷\\n12 │ ┌ @include media-breakpoint-up(xs)\\n13 │ │   .video-card\\n14 │ └     padding-bottom: 55.5%\\n   ╵\\n  app/assets/stylesheets/video.sass 12:1        @import\\n  app/assets/stylesheets/application.sass 30:9  root stylesheet\\n    at tryRunOrWebpackError (/app/node_modules/webpack/lib/HookWebpackError.js:86:9)\\n    at __webpack_require_module__ (/app/node_modules/webpack/lib/Compilation.js:5276:12)\\n    at __webpack_require__ (/app/node_modules/webpack/lib/Compilation.js:5233:18)\\n    at /app/node_modules/webpack/lib/Compilation.js:5305:20\\n    at symbolIterator (/app/node_modules/neo-async/async.js:3485:9)\\n    at done (/app/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/app/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/app/node_modules/tapable/lib/Hook.js:18:14)\\n    at /app/node_modules/webpack/lib/Compilation.js:5211:43\\n    at symbolIterator (/app/node_modules/neo-async/async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nUndefined mixin.\\n   ╷\\n12 │ ┌ @include media-breakpoint-up(xs)\\n13 │ │   .video-card\\n14 │ └     padding-bottom: 55.5%\\n   ╵\\n  app/assets/stylesheets/video.sass 12:1        @import\\n  app/assets/stylesheets/application.sass 30:9  root stylesheet\\n    at Object.<anonymous> (/app/node_modules/css-loader/dist/cjs.js!/app/node_modules/sass-loader/dist/cjs.js!/app/app/assets/stylesheets/application.sass:1:7)\\n    at /app/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:453:10\\n    at Hook.eval [as call] (eval at create (/app/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (/app/node_modules/tapable/lib/Hook.js:14:14)\\n    at /app/node_modules/webpack/lib/Compilation.js:5278:39\\n    at tryRunOrWebpackError (/app/node_modules/webpack/lib/HookWebpackError.js:81:7)\\n    at __webpack_require_module__ (/app/node_modules/webpack/lib/Compilation.js:5276:12)\\n    at __webpack_require__ (/app/node_modules/webpack/lib/Compilation.js:5233:18)\\n    at /app/node_modules/webpack/lib/Compilation.js:5305:20\\n    at symbolIterator (/app/node_modules/neo-async/async.js:3485:9)\\n\\nGenerated code for /app/node_modules/css-loader/dist/cjs.js!/app/node_modules/sass-loader/dist/cjs.js!/app/app/assets/stylesheets/application.sass\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nUndefined mixin.\\\\n   ╷\\\\n12 │ ┌ @include media-breakpoint-up(xs)\\\\n13 │ │   .video-card\\\\n14 │ └     padding-bottom: 55.5%\\\\n   ╵\\\\n  app/assets/stylesheets/video.sass 12:1        @import\\\\n  app/assets/stylesheets/application.sass 30:9  root stylesheet\\\");\");\n\n//# sourceURL=webpack://splits-io/./app/assets/stylesheets/application.sass?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://splits-io/./app/assets/stylesheets/application.sass?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	__webpack_modules__["./app/javascript/application.js"]();
+/******/ 	__webpack_modules__["./app/javascript/application.js"](0, {}, __webpack_require__);
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./app/assets/stylesheets/application.sass"]();
+/******/ 	__webpack_modules__["./app/assets/stylesheets/application.sass"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
