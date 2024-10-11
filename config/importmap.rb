bdb244f69ef6344c666ac65e5b25c659361fa9ab
glacials <ben@twos.dev>
2024-10-11 13:29:49 -0700

    progress

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,8 +8,6 @@ pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "bootstrap" # @5.3.3
 pin "chartkick" # @5.0.1
 pin "jquery" # @3.7.1
-pin "vue", preload: true # @3.5.10
+pin "vue" # @3.5.10
 pin "vue-turbolinks" # @2.2.2
 pin "vue-tippy" # @6.4.4
-
-pin_all_from "app/javascript/vue", to: "vue", under: "vue"
