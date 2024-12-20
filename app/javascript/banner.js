20ee793dee664b07ee03fb2abdafe70b55f67f77
glacials <ben@twos.dev>
2024-12-20 14:51:15 -0800

    Add AGDQ 2025 banner

--- app/javascript/banner.js
+++ app/javascript/banner.js
@@ -10,9 +10,13 @@
 // place a button with the class "banner-close".
 // That's it!
 //
+// To limit a banner to a specific time period,
+// add data-from and data-to attributeseach containing an RFC3339 date string.
+// The banner will only be considered for display if the current date is between the two dates.
+//
 // Example in Slim:
 //
-// .banner#my-banner-name style="display: none"
+// .banner#my-banner-name style="display: none" data={from: "2025-01-05T09:00:00-08:00", to: "2025-01-12T00:15:00-08:00"}
 //   | This is a banner.
 //   button.btn.btn-secondary.banner-close Close
 
@@ -21,6 +25,15 @@ const initBanners = function () {
     const storageKey = element.id;
     if (localStorage.getItem(storageKey) === "hide") {
       return;
+    }
+    if (element.dataset.from && element.dataset.to) {
+      const from = new Date(element.dataset.from);
+      const to = new Date(element.dataset.to);
+      to.setDate(to.getDate() + 1); // Make the end date inclusive
+      const now = new Date();
+      if (now < from || now > to) {
+        continue;
+      }
     }
     element.style.display = "block";
   }
